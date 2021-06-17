756cae3d9256109e6b724bdbdd0159d83d3f3e76
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-17 11:13:45 +0700

    Fix linting

--- src/al/findLongestSubArrLargerThanX.ts
+++ src/al/findLongestSubArrLargerThanX.ts
@@ -20,12 +20,12 @@ export function findLongestSubArrLargerThanX(
   let countZero = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= x && start == -1) {
+    if (arr[i] >= x && start === -1) {
       countZero = 0;
       start = i;
-    } else if (arr[i] == 0) {
+    } else if (arr[i] === 0) {
       countZero++;
-      if (countZero == maxConsecutiveZero) {
+      if (countZero === maxConsecutiveZero) {
         let tempStart = start;
         let tempEnd = i - maxConsecutiveZero;
         if (tempEnd - tempStart > maxEnd - maxStart) {
@@ -35,20 +35,20 @@ export function findLongestSubArrLargerThanX(
         start = -1;
         end = -1;
       }
-    } else if (arr[i] < x && start != -1) {
+    } else if (arr[i] < x && start !== -1) {
       end = i - 1;
     }
   }
 
-  if (start != -1 && end == -1) {
+  if (start !== -1 && end === -1) {
     end = arr.length - 1;
   }
 
-  if (maxStart == -1 || end - start > maxEnd - maxStart) {
+  if (maxStart === -1 || end - start > maxEnd - maxStart) {
     maxStart = start;
   }
 
-  if (maxEnd == -1 || end - start > maxEnd - maxStart) {
+  if (maxEnd === -1 || end - start > maxEnd - maxStart) {
     maxEnd = end;
   }
 
