5c695cebadc0c271194423f5da27523fcab377b1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-24 17:39:26 +0700

    Update dependencies 🎉

---
+++ src/al/twoSum.test.ts
@@ -0,0 +1,19 @@
+import { twoSum } from "./twoSum";
+
+describe("twoSum", () => {
+  it("should work 1 as expected", () => {
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    const expected = [0, 1];
+
+    expect(twoSum(nums, target)).toEqual(expected);
+  });
+
+  it("should work 1 as expected", () => {
+    const nums = [3, 3];
+    const target = 6;
+    const expected = [0, 1];
+
+    expect(twoSum(nums, target)).toEqual(expected);
+  });
+});
