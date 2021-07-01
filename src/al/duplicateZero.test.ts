0407794a8cfa8821fa19dd38ec1510fb7ebc6f33
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-01 17:30:27 +0700

    Solve duplicateZeros

---
+++ src/al/duplicateZero.test.ts
@@ -0,0 +1,17 @@
+import { duplicateZeros } from './duplicateZero';
+
+describe('duplicateZero', () => {
+  it('should work for the first case', () => {
+    const input = [1, 0, 2, 3, 0, 4, 5, 0];
+
+    duplicateZeros(input);
+    expect(input).toEqual([1, 0, 0, 2, 3, 0, 0, 4]);
+  });
+
+  it('should work for the second case', () => {
+    const input = [1, 2, 3];
+
+    duplicateZeros(input);
+    expect(input).toEqual([1, 2, 3]);
+  });
+});
