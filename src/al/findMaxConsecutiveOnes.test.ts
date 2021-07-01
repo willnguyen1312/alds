6986d5235cd3f6dfc5ab595007b3934687d89cbf
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-01 17:01:36 +0700

    Solve findMaxConsecutiveOnes

---
+++ src/al/findMaxConsecutiveOnes.test.ts
@@ -0,0 +1,13 @@
+import { findMaxConsecutiveOnes } from './findMaxConsecutiveOnes';
+
+describe('Name of the group', () => {
+  it('should work for the first case', () => {
+    const input = [1, 1, 0, 1, 1, 1];
+    expect(findMaxConsecutiveOnes(input)).toBe(3);
+  });
+
+  it('should work for the second case', () => {
+    const input = [1, 0, 1, 1, 0, 1];
+    expect(findMaxConsecutiveOnes(input)).toBe(2);
+  });
+});
