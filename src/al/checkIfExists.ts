e9eccd0565e805b7a7dbd875b8d94d1dd10b966e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-02 08:50:20 +0700

    Solve checkIfExist

---
+++ src/al/checkIfExists.ts
@@ -0,0 +1,16 @@
+export function checkIfExist(arr: number[]): boolean {
+  const numSet = new Set<number>();
+
+  for (const num of arr) {
+    if (numSet.has(num / 2)) {
+      return true;
+    }
+
+    if (numSet.has(num * 2)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+
+  return false;
+}
