0feebbc0208089810e50af7307bf3558d1bb784c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-02 08:01:29 +0700

    Solve removeElement

---
+++ src/al/removeElement.test.ts
@@ -0,0 +1,27 @@
+import { removeElement } from './removeElement';
+
+describe('removeElement', () => {
+  it('should work for the first case', () => {
+    const input = [3, 2, 2, 3];
+    const result = 2;
+
+    expect(removeElement(input, 3)).toBe(result);
+    expect(input.slice(0, result)).toEqual([2, 2]);
+  });
+
+  it('should work for the second case', () => {
+    const input = [0, 1, 2, 2, 3, 0, 4, 2];
+    const result = 5;
+
+    expect(removeElement(input, 2)).toBe(result);
+    expect(input.slice(0, result)).toEqual([0, 1, 3, 0, 4]);
+  });
+
+  it('should work for the third case', () => {
+    const input = [0, 4, 4, 0, 4, 4, 4, 0, 2];
+    const result = 4;
+
+    expect(removeElement(input, 4)).toBe(result);
+    expect(input.slice(0, result)).toEqual([0, 0, 0, 2]);
+  });
+});
---
+++ src/al/removeElement.ts
@@ -0,0 +1,15 @@
+export function removeElement(nums: number[], val: number): number {
+  let i = 0;
+  let j = 0;
+
+  while (j < nums.length) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+
+    j++;
+  }
+
+  return i;
+}
