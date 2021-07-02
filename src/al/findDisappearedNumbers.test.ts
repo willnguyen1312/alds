839ecffd33c3b5621c42c802573a7c2a271bb432
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-02 11:51:40 +0700

    Solve

---
+++ src/al/findDisappearedNumbers.test.ts
@@ -0,0 +1,7 @@
+import { findDisappearedNumbers } from './findDisappearedNumbers';
+
+describe('findDisappearedNumbers', () => {
+  it('should work for the fist case', () => {
+    expect(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])).toEqual([5, 6]);
+  });
+});
---
+++ src/al/findDisappearedNumbers.ts
@@ -0,0 +1,18 @@
+export function findDisappearedNumbers(nums: number[]): number[] {
+  const result = [];
+
+  for (const number of nums) {
+    const index = Math.abs(number) - 1;
+    if (nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+
+  return result;
+}
