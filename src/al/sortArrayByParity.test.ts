e81da99398e36d015a9b414f473234ac0e10c3a2
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-02 10:54:14 +0700

    Solve sortArrayByParity

---
+++ src/al/sortArrayByParity.test.ts
@@ -0,0 +1,7 @@
+import { sortArrayByParity } from './sortArrayByParity';
+
+describe('sortArrayByParity', () => {
+  it('should work for the first case', () => {
+    expect(sortArrayByParity([3, 1, 2, 4])).toEqual([2, 4, 1, 3]);
+  });
+});
