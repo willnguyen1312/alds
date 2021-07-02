c0a422bf80d165bbc1125e91e1b2fd76fbc9728f
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-02 18:36:17 +0700

    Solve isPalindromeLinkedList

---
+++ src/al/isPalindromeLinkedList.ts
@@ -0,0 +1,27 @@
+function isPalindrome(head: any): boolean {
+  if (head === null || head.next === null) return true;
+  let slow: any = head,
+    fast: any = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  let reversed: any = reverse(slow);
+  while (head !== slow && head?.val === reversed?.val) {
+    head = head?.next;
+    reversed = reversed?.next;
+  }
+  return head === slow;
+}
+
+function reverse(node: any) {
+  let dummy = new ListNode(-1),
+    next;
+  while (node !== null) {
+    next = node.next;
+    node.next = dummy.next;
+    dummy.next = node;
+    node = next;
+  }
+  return dummy.next;
+}
