193ff9dca3a168adda5ab8f32e922a5e776e6b60
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-02 09:10:19 +0700

    Solve validMountainArray

---
+++ src/al/validMountainArray.test.ts
@@ -0,0 +1,15 @@
+import { validMountainArray } from './validMountainArray';
+
+describe('validMountainArray', () => {
+  it('should work for the first case', () => {
+    expect(validMountainArray([0, 3, 2, 1])).toBe(true);
+  });
+
+  it('should work for the second case', () => {
+    expect(validMountainArray([0, 1, 4, 3, 6, 5, 2, 1])).toBe(false);
+  });
+
+  it('should work for the third case', () => {
+    expect(validMountainArray([3, 5, 5])).toBe(false);
+  });
+});
