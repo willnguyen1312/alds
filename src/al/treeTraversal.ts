adb5b4093e64953f1fbc38fc189e21bf9e34a9c1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-04 12:46:50 +0700

    Add treeTraversal

---
+++ src/al/treeTraversal.ts
@@ -0,0 +1,27 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function preorderTraversal(root: TreeNode | null): number[] {
+  const list: number[] = [];
+
+  getListRecursive(root, list);
+
+  return list;
+}
+
+function getListRecursive(node: TreeNode | null, list: number[]) {
+  if (node === null) {
+    return;
+  }
+  list.push(node.val);
+  getListRecursive(node.left, list);
+  getListRecursive(node.right, list);
+}
