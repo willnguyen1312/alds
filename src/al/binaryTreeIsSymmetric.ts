3dba87852e88747dc2798161c480036a46e5d569
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-04 13:28:19 +0700

    Add binaryTreeIsSymmetric

---
+++ src/al/binaryTreeIsSymmetric.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+  if (root === null) return true;
+  return checkSymmetric(root.left, root.right);
+}
+
+function checkSymmetric(l: any, r: any) {
+  if (l === null && r === null) {
+    return true;
+  } else if (r === null || l === null) {
+    return false;
+  }
+
+  if (l.val !== r.val) return false;
+
+  if (!checkSymmetric(l.left, r.right)) return false;
+  if (!checkSymmetric(l.right, r.left)) return false;
+
+  return true;
+}
