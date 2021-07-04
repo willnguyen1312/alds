0dee1ab368f376dad3640befa9e842fe86742544
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-04 13:08:30 +0700

    Solve levelOrder

---
+++ src/al/levelOrder.ts
@@ -0,0 +1,20 @@
+function levelOrder(root: TreeNode | null): number[][] {
+  let q = [root];
+  let result = [];
+
+  while (q[0]) {
+    let length = q.length;
+    let input = [];
+
+    for (let i = 0; i < length; i++) {
+      let curr: any = q.shift();
+      input.push(curr.val);
+      if (curr.left) q.push(curr.left);
+      if (curr.right) q.push(curr.right);
+    }
+
+    result.push(input);
+  }
+
+  return result;
+}
