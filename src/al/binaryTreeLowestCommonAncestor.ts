c30ef260a616e6316bb669e63e47875fba7dddeb
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-04 17:26:00 +0700

    Add binaryTreeLowestCommonAncestor

---
+++ src/al/binaryTreeLowestCommonAncestor.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  if (root === null) return root;
+  if (p?.val === root.val) return p;
+  if (q?.val === root.val) return q;
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+
+  if (left !== null && right !== null) return root;
+
+  return left !== null ? left : right;
+}
