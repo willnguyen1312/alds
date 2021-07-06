b334143db58152251f14fab6438dcdcd7b087422
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-06 14:40:51 +0700

    Add dailyTemperatures

---
+++ src/al/dailyTemperatures.ts
@@ -0,0 +1,24 @@
+const dailyTemperatures = function (temperatures: number[]) {
+  let tempStack = [temperatures.length - 1];
+  let daysUntilWarmer = new Array(temperatures.length);
+  daysUntilWarmer.fill(0);
+
+  for (let day = temperatures.length - 2; day >= 0; day--) {
+    while (
+      tempStack.length > 0 &&
+      temperatures[day] >= temperatures[tempStack[tempStack.length - 1]]
+    ) {
+      tempStack.pop();
+    }
+
+    if (tempStack.length > 0) {
+      daysUntilWarmer[day] = tempStack[tempStack.length - 1] - day;
+    }
+    tempStack.push(day);
+  }
+
+  return daysUntilWarmer;
+};
+
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
