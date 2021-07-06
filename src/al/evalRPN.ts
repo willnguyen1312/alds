a722f2539f2658382f614421c597f7519de84266
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-06 14:54:05 +0700

    Add evalRPN

---
+++ src/al/evalRPN.ts
@@ -0,0 +1,23 @@
+const evalRPN = (tokens: string[]) => {
+  const operands = new Set(['+', '-', '*', '/']);
+  const stack = [];
+
+  for (let s of tokens) {
+    if (operands.has(s)) {
+      const n2 = parseInt(stack.pop());
+      const n1 = parseInt(stack.pop());
+      stack.push(evaluate(n1, n2, s));
+    } else {
+      stack.push(s);
+    }
+  }
+
+  return parseInt(stack.pop());
+};
+
+const evaluate = (n1: number, n2: number, operand: string): any => {
+  if (operand === '+') return n1 + n2;
+  if (operand === '-') return n1 - n2;
+  if (operand === '*') return n1 * n2;
+  if (operand === '/') return n1 / n2;
+};
