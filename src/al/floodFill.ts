d1a689676f95f4e54bd2b3b8fb781e83386e43ad
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-06 17:44:12 +0700

    Add floodFill

---
+++ src/al/floodFill.ts
@@ -0,0 +1,36 @@
+function floodFill(
+  image: number[][],
+  sr: number,
+  sc: number,
+  newColor: number
+): number[][] {
+  const startingColor = image[sr][sc];
+  image[sr][sc] = newColor;
+  const DIR = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  const helper = (sr: number, sc: number) => {
+    for (let d of DIR) {
+      const row = sr + d[0];
+      const col = sc + d[1];
+      if (
+        row >= 0 &&
+        row < image.length &&
+        col >= 0 &&
+        col < image[row].length &&
+        image[row][col] === startingColor &&
+        image[row][col] !== newColor
+      ) {
+        image[row][col] = newColor;
+        helper(row, col);
+      }
+    }
+  };
+
+  helper(sr, sc);
+  return image;
+}
