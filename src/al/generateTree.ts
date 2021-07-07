b61812bad62a43cca402ccd2be43a256708e8d7a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-07 17:26:36 +0700

    Add sample

--- src/al/generateTree.ts
+++ src/al/generateTree.ts
@@ -29,3 +29,9 @@ function helper(low: number, high: number): Array<TreeNode | null> {
 
   return result;
 }
+
+// Input: n = 3
+// Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+
+// Input: n = 1
+// Output: [[1]]
