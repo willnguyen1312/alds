108d5f7ba0767f8f3b616085b50cf066c168b95a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-07 14:58:27 +0700

    Add swapPairs

---
+++ src/al/swapPairs.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  const nextNode = head.next;
+  head.next = head.next.next;
+  nextNode.next = head;
+  head.next = swapPairs(nextNode.next.next);
+  return nextNode;
+}
