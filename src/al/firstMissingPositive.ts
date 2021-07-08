c6f1c66caeb72683122edc1e7f70bd12a541e5e1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-08 08:02:21 +0700

    Update firstMissingPositive

--- src/al/firstMissingPositive.ts
+++ src/al/firstMissingPositive.ts
@@ -1,12 +1,13 @@
+// https://www.youtube.com/watch?v=-lfHWWMmXXM
 export function firstMissingPositive(nums: number[]): number {
   const n = nums.length;
+
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0 && nums[i] <= n) {
-      const pos = nums[i] - 1;
-      if (nums[pos] !== nums[i]) {
-        [nums[pos], nums[i]] = [nums[i], nums[pos]];
-        i--;
-      }
+    let correctPosition = nums[i] - 1;
+
+    while (nums[i] > 0 && nums[i] <= n && nums[correctPosition] !== nums[i]) {
+      [nums[correctPosition], nums[i]] = [nums[i], nums[correctPosition]];
+      correctPosition = nums[i] - 1;
     }
   }
   for (let i = 0; i < n; i++) if (nums[i] !== i + 1) return i + 1;
