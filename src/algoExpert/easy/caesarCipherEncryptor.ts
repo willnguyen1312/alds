94bb05278e64b7e134a13e8fe799dba056d9b439
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-19 17:51:18 +0700

    caesarCipherEncryptor

---
+++ src/algoExpert/easy/caesarCipherEncryptor.ts
@@ -0,0 +1,16 @@
+export function caesarCipherEncryptor(str: string, key: number): string {
+  const newLetters: string[] = [];
+  const newKey = key & 26;
+  for (const letter of str) {
+    newLetters.push(getNewLetter(letter, newKey));
+  }
+
+  return newLetters.join('');
+}
+
+function getNewLetter(letter: string, key: number) {
+  const newLetterCode = letter.charCodeAt(0) + key;
+  return newLetterCode <= 122
+    ? String.fromCharCode(newLetterCode)
+    : String.fromCharCode(96 + (newLetterCode % 122));
+}
