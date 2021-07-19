d4dcc84f65ddc9e8dd81a66f1f853dac7cd312b5
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-19 15:12:11 +0700

    Add validateSubsequence

---
+++ src/algoExpert/easy/validateSubsequence.ts
@@ -0,0 +1,16 @@
+export function validateSubsequence(
+  arr: number[],
+  sequence: number[]
+): boolean {
+  let arrIndex = 0;
+  let seqIndex = 0;
+
+  while (arrIndex < arr.length && seqIndex < sequence.length) {
+    if (sequence[seqIndex] === arr[arrIndex]) {
+      seqIndex++;
+    }
+    arrIndex++;
+  }
+
+  return seqIndex === sequence.length;
+}
