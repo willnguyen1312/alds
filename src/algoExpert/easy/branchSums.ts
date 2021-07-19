4a259e3787e480f526aeae2fe36152bcdb645bb9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-19 15:46:30 +0700

    Add branchSums

---
+++ src/algoExpert/easy/branchSums.ts
@@ -0,0 +1,20 @@
+export function branchSums(root: TreeNode): number[] {
+  const result: number[] = [];
+  calculateBranchSum(root, 0, result);
+  return result;
+}
+
+function calculateBranchSum(
+  node: TreeNode,
+  currentSum: number,
+  sums: number[]
+) {
+  const newSum = currentSum + node.val;
+  if (!node.left && !node.right) {
+    sums.push(newSum);
+    return;
+  }
+
+  node.left && calculateBranchSum(node.left, newSum, sums);
+  node.right && calculateBranchSum(node.right, newSum, sums);
+}
