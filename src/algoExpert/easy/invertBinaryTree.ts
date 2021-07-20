6e07170242ace89b11072432c7dad4a4e9c4870e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 14:43:48 +0700

    Revert

---
+++ src/algoExpert/easy/invertBinaryTree.ts
@@ -0,0 +1,24 @@
+function invertBinaryTree(tree: TreeNode) {
+  const queue = [tree];
+
+  while (queue.length) {
+    const current = queue.shift();
+    if (!current) continue;
+
+    swapLeftAndRight(current);
+    current.left && queue.push(current.left);
+    current.right && queue.push(current.right);
+  }
+}
+
+function invertBinaryTreeRecur(tree: TreeNode) {
+  swapLeftAndRight(tree);
+  tree.left && invertBinaryTreeRecur(tree.left);
+  tree.right && invertBinaryTreeRecur(tree.right);
+}
+
+function swapLeftAndRight(tree: TreeNode) {
+  const left = tree.left;
+  tree.left = tree.right;
+  tree.right = left;
+}
