b11de435b45d649938e4949c865cf2f5c1b06038
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 16:12:31 +0700

    Add levenshteinDistance

---
+++ src/algoExpert/medium/levenshteinDistance.ts
@@ -0,0 +1,59 @@
+function levenshteinDistance(str1: string, str2: string) {
+  const small = str1.length < str2.length ? str1 : str2;
+  const big = str1.length >= str2.length ? str1 : str2;
+
+  const evenEdits = [];
+  const oddEdits = new Array(small.length + 1);
+  for (let j = 0; j < small.length + 1; j++) {
+    evenEdits.push(j);
+  }
+
+  for (let i = 1; i < big.length + 1; i++) {
+    let currentEdits, previousEdits;
+    if (i % 2 === 1) {
+      currentEdits = oddEdits;
+      previousEdits = evenEdits;
+    } else {
+      currentEdits = evenEdits;
+      previousEdits = oddEdits;
+    }
+
+    currentEdits[0] = i;
+    for (let j = 1; j < small.length + 1; j++) {
+      if (big[i - 1] === small[j - 1]) {
+        currentEdits[j] = previousEdits[j - 1];
+      } else {
+        currentEdits[j] =
+          1 +
+          Math.min(previousEdits[j - 1], previousEdits[j], currentEdits[j - 1]);
+      }
+    }
+  }
+
+  return big.length % 2 === 0
+    ? evenEdits[small.length]
+    : oddEdits[small.length];
+}
+
+function levenshteinDistanceSimpler(str1: string, str2: string) {
+  const edits = [];
+  for (let i = 0; i < str2.length + 1; i++) {
+    const row = [];
+    for (let j = 0; j < str1.length + 1; j++) {
+      row.push(j);
+    }
+    row[0] = i;
+    edits.push(row);
+  }
+  for (let i = 1; i < str2.length + 1; i++) {
+    for (let j = 1; j < str1.length + 1; j++) {
+      if (str2[i - 1] === str1[j - 1]) {
+        edits[i][j] = edits[i - 1][j - 1];
+      } else {
+        edits[i][j] =
+          1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i - 1][j]);
+      }
+    }
+  }
+  return edits[str2.length][str1.length];
+}
