5bbe1f50716837cd2964ac9e6660910a127ec9b8
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 14:16:30 +0700

    Add validateBST

---
+++ src/algoExpert/easy/validateBST.ts
@@ -0,0 +1,17 @@
+export function validateBST(node: TreeNode): boolean {
+  return validateBstHelper(node, -Infinity, Infinity);
+}
+
+function validateBstHelper(
+  tree: TreeNode | null,
+  minValue: number,
+  maxValue: number
+): boolean {
+  if (!tree) return true;
+
+  if (tree.val < minValue || tree.val > maxValue) return false;
+  const leftIsValid = validateBstHelper(tree.left, minValue, tree.val);
+  const rightIsValid = validateBstHelper(tree.right, tree.val, maxValue);
+
+  return leftIsValid && rightIsValid;
+}
