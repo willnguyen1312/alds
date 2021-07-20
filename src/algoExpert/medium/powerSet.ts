72e1d01205b9f9002d214216d691cfe82b766c6a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 17:50:23 +0700

    Add powerset

---
+++ src/algoExpert/medium/powerSet.ts
@@ -0,0 +1,33 @@
+export function powerset(array: number[], idx: number): number[][] {
+  if (!idx) {
+    idx = array.length - 1;
+  }
+
+  if (idx < 0) {
+    return [[]];
+  }
+
+  const ele = array[idx];
+  const subsets = powerset(array, idx - 1);
+  const length = subsets.length;
+
+  for (let i = 0; i < length; i++) {
+    const currentSubset = subsets[i];
+    subsets.push(currentSubset.concat(ele));
+  }
+
+  return subsets;
+}
+
+export function powerset2(array: number[]): number[][] {
+  const subsets: number[][] = [[]];
+
+  for (const ele of array) {
+    for (let i = 0; i < subsets.length; i++) {
+      const currentSubset = subsets[i];
+      subsets.push(currentSubset.concat(ele));
+    }
+  }
+
+  return subsets;
+}
