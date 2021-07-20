e8ba0d09d77928fcfa153b71e152640460f39d2a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 16:34:17 +0700

    Add singleCycleCheck

---
+++ src/algoExpert/medium/singleCycleCheck.ts
@@ -0,0 +1,19 @@
+export function singleCycleCheck(array: number[]): boolean {
+  let numElementsVisited = 0;
+  let currentIdx = 0;
+
+  while (numElementsVisited < array.length) {
+    if (numElementsVisited > 0 && currentIdx === 0) return false;
+
+    numElementsVisited++;
+    currentIdx = getNextIdx(currentIdx, array);
+  }
+
+  return currentIdx === 0;
+}
+
+function getNextIdx(currentIdx: number, array: number[]) {
+  const jump = array[currentIdx];
+  const nextIdx = (currentIdx + jump) % array.length;
+  return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}
