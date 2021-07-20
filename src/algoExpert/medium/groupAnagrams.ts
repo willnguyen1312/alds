434395bcfec1c450dc8e0c8e0d1072b92e0b416a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 18:20:34 +0700

    Add groupAnagrams

---
+++ src/algoExpert/medium/groupAnagrams.ts
@@ -0,0 +1,14 @@
+export function groupAnagrams(str: string[]): string[][] {
+  const anagrams: Record<string, string[]> = {};
+
+  for (const word of str) {
+    const sortedWord = word.split('').sort().join('');
+    if (sortedWord in anagrams) {
+      anagrams[sortedWord].push(word);
+    } else {
+      anagrams[sortedWord] = [word];
+    }
+  }
+
+  return Object.values(anagrams);
+}
