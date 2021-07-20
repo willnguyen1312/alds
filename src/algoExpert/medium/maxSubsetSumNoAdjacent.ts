ede35312a96dad019de6438a6254fe617b8baebf
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-20 15:10:50 +0700

    Add maxSubsetSumNoAdjacent

---
+++ src/algoExpert/medium/maxSubsetSumNoAdjacent.ts
@@ -0,0 +1,30 @@
+export function maxSubsetSumNoAdjacent(array: number[]): number {
+  if (!array.length) return 0;
+
+  if (array.length === 1) return array[0];
+
+  const maxSums = array.slice();
+  maxSums[1] = Math.max(array[0], array[1]);
+
+  for (let i = 2; i < array.length; i++) {
+    maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+  }
+
+  return maxSums[maxSums.length - 1];
+}
+
+export function maxSubsetSumNoAdjacent2(array: number[]) {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+
+  for (let i = 2; i < array.length; i++) {
+    const current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+
+  return first;
+}
