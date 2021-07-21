4c783d76a57bf04eb06b902a9d7e2b570e9c2844
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-21 17:25:49 +0700

    Add fourNumberSum

---
+++ src/algoExpert/medium/fourNumberSum.ts
@@ -0,0 +1,27 @@
+function fourNumberSum(array: number[], targetSum: number): number[][] {
+  const allPairSums: Record<number, number[][]> = {};
+  const quadruplets = [];
+
+  for (let i = 1; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const currentSum = array[i] + array[j];
+      const difference = targetSum - currentSum;
+      if (difference in allPairSums) {
+        for (const pair of allPairSums[difference]) {
+          quadruplets.push(pair.concat([array[i], array[j]]));
+        }
+      }
+    }
+
+    for (let k = 0; k < i; k++) {
+      const currentSum = array[i] + array[k];
+      if (!(currentSum in allPairSums)) {
+        allPairSums[currentSum] = [[array[k], array[i]]];
+      } else {
+        allPairSums[currentSum].push([array[k], array[i]]);
+      }
+    }
+  }
+
+  return quadruplets;
+}
