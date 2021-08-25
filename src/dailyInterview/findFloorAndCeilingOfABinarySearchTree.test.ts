2a0151c3a35ed99526927e30fbc8ff3899592281
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-25 14:59:46 +0700

    Solve findFloorAndCeilingOfABinarySearchTree 🍁

---
+++ src/dailyInterview/findFloorAndCeilingOfABinarySearchTree.test.ts
@@ -0,0 +1,19 @@
+import { findCeil, findFloor, findFloorAndCeilingOfABinarySearchTree, BinaryTreeNode } from './findFloorAndCeilingOfABinarySearchTree';
+
+describe('findFloorAndCeilingOfABinarySearchTree function', () => {
+  it('should work', () => {
+    const root = new BinaryTreeNode(8);
+    root.left = new BinaryTreeNode(4);
+    root.right = new BinaryTreeNode(12);
+
+    root.left.left = new BinaryTreeNode(2);
+    root.left.right = new BinaryTreeNode(6);
+
+    root.right.left = new BinaryTreeNode(10);
+    root.right.right = new BinaryTreeNode(14);
+
+    expect(findFloorAndCeilingOfABinarySearchTree(root, 5)).toEqual([4, 6]);
+    expect(findCeil(root, 5)).toBe(6);
+    expect(findFloor(root, 5)).toBe(4);
+  });
+});
---
+++ src/dailyInterview/findFloorAndCeilingOfABinarySearchTree.ts
@@ -0,0 +1,76 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given an integer k and a binary search tree, find the floor (less than or equal to) of k, and the ceiling (larger than or equal to) of k. If either does not exist, then print them as None.
+
+Here is the definition of a node for the tree.
+*/
+
+export class BinaryTreeNode {
+  val: number;
+  left: BinaryTreeNode | null;
+  right: BinaryTreeNode | null;
+
+  constructor(val: number) {
+    this.val = val;
+  }
+}
+
+export function findFloorAndCeilingOfABinarySearchTree(root: BinaryTreeNode, target: number): number[] {
+  let floor = -1;
+  let ceil = -1;
+
+  while (root) {
+    if (root.val === target) {
+      ceil = root.val;
+      floor = root.val;
+      return [floor, ceil];
+    }
+
+    if (target > root.val) {
+      floor = root.val;
+      root = root.right;
+    } else {
+      ceil = root.val;
+      root = root.left;
+    }
+  }
+
+  return [floor, ceil];
+}
+
+export function findCeil(node: BinaryTreeNode, target: number) {
+  if (!node) {
+    return -1;
+  }
+
+  if (node.val === target) {
+    return node.val;
+  }
+
+  if (node.val < target) {
+    return findCeil(node.right, target);
+  }
+
+  let ceil = findCeil(node.left, target);
+
+  return ceil !== -1 ? ceil : node.val;
+}
+
+export function findFloor(node: BinaryTreeNode, target: number) {
+  if (!node) {
+    return -1;
+  }
+
+  if (node.val === target) {
+    return node.val;
+  }
+
+  if (node.val > target) {
+    return findFloor(node.left, target);
+  }
+
+  let floor = findFloor(node.right, target);
+
+  return floor !== -1 ? floor : node.val;
+}
