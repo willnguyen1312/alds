4a8e070864d80a40d37123f71f34aa1f2ed9dcf1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-26 14:00:40 +0700

    Solve removeKthElementFromLinkedList 🍁

---
+++ src/dailyInterview/removeKthElementFromLinkedList.test.ts
@@ -0,0 +1,27 @@
+import { LinkedListNode, removeKthElementFromLinkedList } from './removeKthElementFromLinkedList';
+
+describe('removeKthElementFromLinkedList function', () => {
+  it('should work', () => {
+    const head = new LinkedListNode(1);
+    head.next = new LinkedListNode(2);
+    head.next.next = new LinkedListNode(3);
+    head.next.next.next = new LinkedListNode(4);
+    head.next.next.next.next = new LinkedListNode(5);
+
+    expect(removeKthElementFromLinkedList(head, 2)).toMatchInlineSnapshot(`
+LinkedListNode {
+  "next": LinkedListNode {
+    "next": LinkedListNode {
+      "next": LinkedListNode {
+        "next": null,
+        "val": 5,
+      },
+      "val": 3,
+    },
+    "val": 2,
+  },
+  "val": 1,
+}
+`);
+  });
+});
---
+++ src/dailyInterview/removeKthElementFromLinkedList.ts
@@ -0,0 +1,42 @@
+/*
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+You are given a singly linked list and an integer k.
+Return the linked list, removing the k-th last element from the list. 
+
+Try to do it in a single pass and using constant space.
+*/
+
+export class LinkedListNode {
+  val: number;
+  next: null | LinkedListNode = null;
+  constructor(val: number) {
+    this.val = val;
+  }
+}
+
+export function removeKthElementFromLinkedList(head: LinkedListNode, k: number) {
+  let length = 0;
+  let currentNode = head;
+
+  while (currentNode) {
+    length++;
+    currentNode = currentNode.next;
+  }
+
+  let target = length - k;
+  currentNode = head;
+  let previous: LinkedListNode | null = null;
+
+  while (target !== 0) {
+    previous = currentNode;
+    currentNode = currentNode.next;
+    target--;
+  }
+
+  if (previous) {
+    previous.next = currentNode.next;
+  }
+
+  return head;
+}
