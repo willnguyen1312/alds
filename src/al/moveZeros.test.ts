844f6ab5f7bd3c67aa6d5cfb4bca2e3e59bfccab
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-26 15:48:22 +0700

    Solve findKthLargest 🍁

--- src/al/moveZeros.test.ts
+++
@@ -1,9 +0,0 @@
-import { moveZeroes } from './moveZeros';
-
-describe('moveZeros', () => {
-  it('should work for the first case', () => {
-    const input = [0, 1, 0, 3, 12];
-    moveZeroes(input);
-    expect(input).toEqual([1, 3, 12, 0, 0]);
-  });
-});
--- src/dailyInterview/findKLargest.ts
+++ src/dailyInterview/findKLargest.ts
@@ -1,3 +1,11 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Facebook:
+
+Given a list, find the k-th largest element in the list.
+Input: list = [3, 5, 2, 4, 6, 8], k = 3
+Output: 5
+*/
+
 function getKth(k: number, nums: number[], start: number, end: number): number {
   let pivot = nums[end];
 
