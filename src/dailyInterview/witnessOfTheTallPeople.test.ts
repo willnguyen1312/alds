3ab3282e7a37faba16fec7a82cedcaebdd11d081
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-26 14:21:38 +0700

    Solve witnessOfTheTallPeople 🍁

---
+++ src/dailyInterview/witnessOfTheTallPeople.test.ts
@@ -0,0 +1,7 @@
+import { witnessOfTheTallPeople } from './witnessOfTheTallPeople';
+
+describe('witnessOfTheTallPeople function', () => {
+  it('should work', () => {
+    expect(witnessOfTheTallPeople([3, 6, 3, 4, 1])).toBe(3);
+  });
+});
