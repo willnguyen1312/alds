5e8e7028915395aa3e77a7d07346deb77cd991eb
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-28 13:29:57 +0700

    Solve funckyTree 🎉

--- src/random/funkyTree.test.ts
+++ src/random/funkyTree.test.ts
@@ -10,50 +10,125 @@ const sampleTree: TreeNode[] = [
     children: [
       {
         label: '1.1',
-      },
-      {
-        label: '1.2',
         children: [
           {
-            label: '1.2.3',
+            label: '1.1.1',
           },
         ],
       },
-    ],
+      {
+        label: '1.2',
+      },
+      {
+        label: '1.3',
+        children: [
+          {
+            label: '1.3.1',
+          },
+        ],
+      },
+    ].sort((a, b) => {
+      if (a.label < b.label) {
+        return 1;
+      } else if (a.label > b.label) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }),
   },
 ];
 
-const dfs = (treeNode?: TreeNode, parent?: TreeNode) => {
+const dfsAddParent = (treeNode?: TreeNode, parent?: TreeNode) => {
   if (!treeNode) {
     return;
   }
 
-  treeNode.children?.forEach((node) => dfs(node, treeNode));
+  treeNode.children?.forEach((node) => dfsAddParent(node, treeNode));
   treeNode.parent = parent;
 };
 
 const dfsIter = (treeNode: TreeNode) => {
   const stack: TreeNode[] = [treeNode];
+  const result: string[] = [];
 
   while (stack.length) {
     const current = stack.pop();
-    current?.children?.forEach((node) => stack.push(node));
+    result.push(current.label);
+    current?.children?.reverse().forEach((node) => stack.push(node));
   }
+
+  return result;
+};
+
+const dfsRecur = (tree: TreeNode) => {
+  const result: string[] = [];
+
+  const dfsRecurHelper = (node: TreeNode) => {
+    result.push(node.label);
+
+    node.children?.forEach((child) => {
+      dfsRecurHelper(child);
+    });
+  };
+
+  dfsRecurHelper(tree);
+
+  return result;
 };
 
 const bfs = (treeNode: TreeNode) => {
+  const result: string[] = [];
   const queue: TreeNode[] = [treeNode];
 
   while (queue.length) {
-    queue.forEach((item) => {
-      queue.push(...(item.children || []));
-      queue.shift();
-    });
+    const toQueue: TreeNode[] = [];
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      result.push(node.label);
+      toQueue.push(...(node.children || []));
+    }
+
+    queue.push(...toQueue);
   }
+
+  return result;
 };
 
-describe('funkyTree', () => {
+describe('funkyTree functions', () => {
   it('should work', () => {
-    sampleTree.forEach((node) => bfs(node));
+    expect(bfs(sampleTree[0])).toMatchInlineSnapshot(`
+Array [
+  "1",
+  "1.3",
+  "1.2",
+  "1.1",
+  "1.3.1",
+  "1.1.1",
+]
+`);
+
+    expect(dfsIter(sampleTree[0])).toMatchInlineSnapshot(`
+Array [
+  "1",
+  "1.3",
+  "1.3.1",
+  "1.2",
+  "1.1",
+  "1.1.1",
+]
+`);
+
+    expect(dfsRecur(sampleTree[0])).toMatchInlineSnapshot(`
+Array [
+  "1",
+  "1.1",
+  "1.1.1",
+  "1.2",
+  "1.3",
+  "1.3.1",
+]
+`);
   });
 });
