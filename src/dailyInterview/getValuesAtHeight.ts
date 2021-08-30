de04624c5e84220f2872057c585e30ca1b8a60d0
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-30 18:33:37 +0700

    Solve getValuesAtHeight 🎉

---
+++ src/dailyInterview/getValuesAtHeight.ts
@@ -0,0 +1,35 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Amazon:
+
+Given a binary tree, return all values given a certain height h
+*/
+
+export class BinaryTreeNode {
+  val: number;
+  left: BinaryTreeNode | null;
+  right: BinaryTreeNode | null;
+
+  constructor(val: number) {
+    this.val = val;
+  }
+}
+
+export function getValuesAtHeight(root: BinaryTreeNode, height: number): number[] {
+  const result: number[] = [];
+
+  function collect(node: BinaryTreeNode | null, currentHeight: number = 0) {
+    if (node) {
+      collect(node.left, ++currentHeight);
+
+      if (currentHeight === height) {
+        result.push(node.val);
+      }
+
+      collect(node.right, currentHeight);
+    }
+  }
+
+  collect(root);
+
+  return result;
+}
