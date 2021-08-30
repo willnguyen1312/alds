9911d60fbfdcb248dfb0bae913e243915031940f
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-30 14:44:56 +0700

    Solve queueByTwoStacks 🎉

---
+++ src/dailyInterview/queueByTwoStacks.ts
@@ -0,0 +1,24 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Implement a queue class using two stacks.
+A queue is a data structure that supports the FIFO protocol (First in = first out).
+Your class should support the enqueue and dequeue methods like a standard queue.
+*/
+
+export class Queue {
+  inStack: number[] = [];
+  outStack: number[] = [];
+
+  enQueue(value: number) {
+    this.inStack.push(value);
+  }
+
+  deQueue() {
+    while (this.inStack.length) {
+      this.outStack.push(this.inStack.pop());
+    }
+
+    return this.outStack.pop();
+  }
+}
