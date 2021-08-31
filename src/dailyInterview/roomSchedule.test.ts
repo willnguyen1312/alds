b72275eb9621e570a2002828207770629a8090c9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-31 16:44:50 +0700

    Solve roomSchedule 🎉

---
+++ src/dailyInterview/roomSchedule.test.ts
@@ -0,0 +1,13 @@
+import { roomSchedule } from './roomSchedule';
+
+describe('roomSchedule function', () => {
+  it('should work', () => {
+    expect(
+      roomSchedule([
+        [30, 75],
+        [0, 50],
+        [60, 150],
+      ])
+    ).toBe(1);
+  });
+});
---
+++ src/dailyInterview/roomSchedule.ts
@@ -0,0 +1,29 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Google:
+
+You are given an array of tuples (start, end) representing time intervals for lectures.
+The intervals may be overlapping. Return the number of rooms that are required.
+
+For example. [(30, 75), (0, 50), (60, 150)] should return 2.
+*/
+
+export function roomSchedule(arr: number[][]): number {
+  const sortedArr = arr.sort((a, b) => a[0] - b[0]);
+  const mergedArr: number[][] = [];
+
+  let current = sortedArr[0];
+
+  for (let index = 1; index < sortedArr.length; index++) {
+    const element = sortedArr[index];
+
+    if (current[1] < element[0]) {
+      mergedArr.push(current);
+      current = element;
+    } else {
+      current[1] = Math.max(current[1], element[1]);
+    }
+  }
+
+  mergedArr.push(current);
+  return mergedArr.length;
+}
