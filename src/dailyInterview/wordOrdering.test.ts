0a353dc22839e5e220121694004dc9e28a199f1f
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-08-31 14:34:51 +0700

    Solve wordOrdering 🎉

---
+++ src/dailyInterview/wordOrdering.test.ts
@@ -0,0 +1,8 @@
+import { wordOrdering } from './wordOrdering';
+
+describe('wordOrdering function', () => {
+  it('should work', () => {
+    expect(wordOrdering(['abcd', 'efgh'], 'zyxwvutsrqponmlkjihgfedcba')).toBe(false);
+    expect(wordOrdering(['zyx', 'zyxw', 'zyxwy'], 'zyxwvutsrqponmlkjihgfedcba')).toBe(true);
+  });
+});
---
+++ src/dailyInterview/wordOrdering.ts
@@ -0,0 +1,44 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given a list of words, and an arbitrary alphabetical order,
+verify that the words are in order of the alphabetical order.
+
+Example:
+Input:
+words = ["abcd", "efgh"], order="zyxwvutsrqponmlkjihgfedcba"
+
+Output: False
+Explanation: 'e' comes before 'a' so 'efgh' should come before 'abcd'
+
+Example 2:
+Input:
+words = ["zyx", "zyxw", "zyxwy"],
+order="zyxwvutsrqponmlkjihgfedcba"
+Explanation: The words are in increasing alphabetical order
+
+Output: True
+
+Explanation: 'e' comes before 'a' so 'efgh' should come before 'abcd'
+*/
+
+export function wordOrdering(words: string[], order: string): boolean {
+  const lookup = order.split('').reduce((acc: Record<string, number>, cur, index) => {
+    return Object.assign(acc, {
+      [cur]: index,
+    });
+  }, {});
+
+  for (let i = 1; i < words.length; i++) {
+    const cur = words[i];
+    const prev = words[i - 1];
+
+    for (let j = 0; j < Math.min(cur.length, prev.length); j++) {
+      if (lookup[prev[j]] > lookup[cur[j]]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
