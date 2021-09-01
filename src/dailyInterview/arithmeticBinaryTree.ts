58ef4babe010ee22cbcd0ecf47e101df7009ff85
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-01 13:08:56 +0700

    Solve arithmeticBinaryTree 🎉

---
+++ src/dailyInterview/arithmeticBinaryTree.ts
@@ -0,0 +1,60 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+You are given a binary tree representation of an arithmetic expression.
+In this tree, each leaf is an integer value,, and a non-leaf node is one of
+the four operations: '+', '-', '*', or '/'.
+
+Write a function that takes this tree and evaluates the expression.
+
+Example:
+
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+
+This is a representation of the expression (3 + 2) * (4 + 5), and should return 45.
+*/
+
+export const enum Action {
+  DIVIDE = '/',
+  MINUS = '-',
+  PLUS = '+',
+  TIMES = '*',
+}
+
+// const actionSet = new Set([
+//   Action.DIVIDE,
+//   Action.MINUS,
+//   Action.PLUS,
+//   Action.TIMES,
+// ]);
+
+export class BinaryTreeNode {
+  val: number | string;
+  left?: BinaryTreeNode;
+  right?: BinaryTreeNode;
+
+  constructor(val: number | string) {
+    this.val = val;
+  }
+}
+
+export function arithmeticBinaryTree(root: BinaryTreeNode): number {
+  const handlers = {
+    [Action.TIMES]: (first: BinaryTreeNode, second: BinaryTreeNode) =>
+      arithmeticBinaryTree(root.left) * arithmeticBinaryTree(root.right),
+    [Action.DIVIDE]: (first: BinaryTreeNode, second: BinaryTreeNode) =>
+      arithmeticBinaryTree(root.left) / arithmeticBinaryTree(root.right),
+    [Action.PLUS]: (first: BinaryTreeNode, second: BinaryTreeNode) =>
+      arithmeticBinaryTree(root.left) + arithmeticBinaryTree(root.right),
+    [Action.MINUS]: (first: BinaryTreeNode, second: BinaryTreeNode) =>
+      arithmeticBinaryTree(root.left) - arithmeticBinaryTree(root.right),
+  };
+
+  const handle = handlers[root.val];
+
+  return handle ? handle(root.left, root.right) : root.val;
+}
