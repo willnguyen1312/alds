57312920c5a57664e70c39b111b6012fc4f517c5
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-02 13:56:06 +0700

    Solve findSmallestNoneSum 🎉

---
+++ src/dailyInterview/findSmallestNoneSum.test.ts
@@ -0,0 +1,7 @@
+import { findSmallestNoneSum } from './findSmallestNoneSum';
+
+describe('findSmallestNoneSum function', () => {
+  it('should work', () => {
+    expect(findSmallestNoneSum([1, 2, 3, 8, 9, 10])).toBe(7);
+  });
+});
---
+++ src/dailyInterview/findSmallestNoneSum.ts
@@ -0,0 +1,22 @@
+/*
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+Given a sorted list of positive numbers, find the smallest positive number
+that cannot be a sum of any subset in the list.
+
+Example:
+Input: [1, 2, 3, 8, 9, 10]
+Output: 7
+*/
+
+export function findSmallestNoneSum(arr: number[]): number {
+  let result = 1;
+
+  // Traverse the array and increment 'res' if arr[i] is
+  // smaller than or equal to 'res'.
+  for (let i = 0; i < arr.length && arr[i] <= result; i++) {
+    result = result + arr[i];
+  }
+
+  return result;
+}
