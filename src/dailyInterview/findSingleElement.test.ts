28c6fd0c428210a1df2fe558ab892452ba163e99
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-02 12:23:05 +0700

    Solve findSingleElement 🎉

---
+++ src/dailyInterview/findSingleElement.test.ts
@@ -0,0 +1,7 @@
+import { findSingleElement } from './findSingleElement';
+
+describe('findSingleElement function', () => {
+  it('should work', () => {
+    expect(findSingleElement([7, 3, 5, 5, 4, 3, 4, 8, 8])).toBe(7);
+  });
+});
---
+++ src/dailyInterview/findSingleElement.ts
@@ -0,0 +1,14 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given an array of integers, arr, where all numbers occur twice except one number which occurs once, find the number. Your solution should ideally be O(n) time and use constant extra space. 
+Example:
+Input: arr = [7, 3, 5, 5, 4, 3, 4, 8, 8]
+Output: 7
+*/
+
+export function findSingleElement(numbs: number[]): number {
+  return numbs.reduce((acc, cur) => {
+    return acc ^ cur;
+  });
+}
