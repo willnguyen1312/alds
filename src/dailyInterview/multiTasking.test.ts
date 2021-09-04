cd5c04deac5ce2f3a22d6d19f278fa99b6e3cd09
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-04 11:16:01 +0700

    Solve multiTasking 🎉

---
+++ src/dailyInterview/multiTasking.test.ts
@@ -0,0 +1,7 @@
+import { multiTasking } from './multiTasking';
+
+describe('multiTasking function', () => {
+  it('should work', () => {
+    expect(multiTasking([1, 1, 2, 1], 2)).toBe(7);
+  });
+});
