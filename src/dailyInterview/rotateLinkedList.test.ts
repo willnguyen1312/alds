c7cbd799121d5ecef37cb093b3035b02734a1b7e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-04 15:49:50 +0700

    Solve rotateLinkedList 🎉

---
+++ src/dailyInterview/rotateLinkedList.test.ts
@@ -0,0 +1,51 @@
+import {
+  LinkedListNode,
+  rotateCounterClockwise,
+  rotateClockwise,
+} from './rotateLinkedList';
+
+describe('rotateLinkedList function', () => {
+  it('should work', () => {
+    const node = new LinkedListNode(
+      1,
+      new LinkedListNode(2, new LinkedListNode(3, new LinkedListNode(4)))
+    );
+
+    expect(rotateCounterClockwise(node, 3)).toMatchInlineSnapshot(`
+LinkedListNode {
+  "next": LinkedListNode {
+    "next": LinkedListNode {
+      "next": LinkedListNode {
+        "next": null,
+        "val": 3,
+      },
+      "val": 2,
+    },
+    "val": 1,
+  },
+  "val": 4,
+}
+`);
+
+    const node2 = new LinkedListNode(
+      1,
+      new LinkedListNode(2, new LinkedListNode(3, new LinkedListNode(4)))
+    );
+
+    expect(rotateClockwise(node2, 3)).toMatchInlineSnapshot(`
+LinkedListNode {
+  "next": LinkedListNode {
+    "next": LinkedListNode {
+      "next": LinkedListNode {
+        "next": null,
+        "val": 1,
+      },
+      "val": 4,
+    },
+    "val": 3,
+  },
+  "val": 2,
+}
+`);
+  });
+});
---
+++ src/dailyInterview/rotateLinkedList.ts
@@ -0,0 +1,83 @@
+/*
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+Given a linked list and a number k, rotate the linked list by k places.
+*/
+
+export class LinkedListNode {
+  val: number;
+  next: LinkedListNode | null;
+  constructor(val?: number, next?: LinkedListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function rotateCounterClockwise(
+  head: LinkedListNode,
+  k: number
+): LinkedListNode {
+  //   if (k === 0) return head;
+
+  //   let current = head;
+
+  //   while (current.next) current = current.next;
+
+  //   current.next = head;
+  //   current = head;
+
+  //   for (let i = 0; i < k - 1; i++) current = current.next;
+
+  //   head = current.next;
+
+  //   return head;
+
+  if (!head) {
+    return null;
+  }
+
+  const queue: LinkedListNode[] = [];
+  let current = head;
+  while (current) {
+    queue.push(current);
+    current = current.next;
+  }
+
+  k = k % queue.length;
+
+  while (k) {
+    queue[queue.length - 1].next = queue[0];
+    queue.push(queue.shift());
+    queue[queue.length - 1].next = null;
+    k--;
+  }
+
+  return queue[0];
+}
+
+export function rotateClockwise(
+  head: LinkedListNode,
+  k: number
+): LinkedListNode {
+  if (!head) {
+    return null;
+  }
+
+  const queue: LinkedListNode[] = [];
+  let current = head;
+  while (current) {
+    queue.push(current);
+    current = current.next;
+  }
+
+  k = k % queue.length;
+
+  while (k) {
+    queue[queue.length - 1].next = queue[0];
+    queue.unshift(queue.pop());
+    queue[queue.length - 1].next = null;
+    k--;
+  }
+
+  return queue[0];
+}
