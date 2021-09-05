21ceea2e4a7806bace5992485313509606f6c67c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-05 15:49:07 +0700

    Solve removeDuplicateLinkedList 🎉

---
+++ src/dailyInterview/removeDuplicateLinkedList.test.ts
@@ -0,0 +1,30 @@
+import {
+  LinkedListNode,
+  removeDuplicateLinkedList,
+} from './removeDuplicateLinkedList';
+
+describe('removeDuplicateLinkedList function', () => {
+  it('should work', () => {
+    const list = new LinkedListNode(
+      1,
+      new LinkedListNode(
+        2,
+        new LinkedListNode(2, new LinkedListNode(3, new LinkedListNode(3)))
+      )
+    );
+
+    const actual = removeDuplicateLinkedList(list);
+    expect(actual).toMatchInlineSnapshot(`
+LinkedListNode {
+  "data": 1,
+  "next": LinkedListNode {
+    "data": 2,
+    "next": LinkedListNode {
+      "data": 3,
+      "next": undefined,
+    },
+  },
+}
+`);
+  });
+});
---
+++ src/dailyInterview/removeDuplicateLinkedList.ts
@@ -0,0 +1,28 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Amazon:
+
+Given a sorted linked list of integers, remove all the duplicate elements in the linked list
+so that all elements in the linked list are unique.
+*/
+
+export class LinkedListNode {
+  constructor(public data: number, public next?: LinkedListNode) {}
+}
+
+export function removeDuplicateLinkedList(
+  head: LinkedListNode
+): LinkedListNode {
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+    while (next && next.next && next.data === next.next.data) {
+      next = next.next;
+    }
+
+    current.next = next;
+    current = next;
+  }
+
+  return head;
+}
