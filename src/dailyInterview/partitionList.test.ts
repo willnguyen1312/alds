c7e648d199b361038471ef080f84d2b08e899e8c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-05 09:52:15 +0700

    Solve partitionList 🎉

---
+++ src/dailyInterview/partitionList.test.ts
@@ -0,0 +1,38 @@
+import { LinkedListNode, partitionList } from './partitionList';
+
+describe('partitionList function', () => {
+  it('should work', () => {
+    const list = new LinkedListNode(
+      1,
+      new LinkedListNode(
+        4,
+        new LinkedListNode(
+          3,
+          new LinkedListNode(2, new LinkedListNode(5, new LinkedListNode(3)))
+        )
+      )
+    );
+
+    expect(partitionList(list, 3)).toMatchInlineSnapshot(`
+LinkedListNode {
+  "data": 1,
+  "next": LinkedListNode {
+    "data": 2,
+    "next": LinkedListNode {
+      "data": 4,
+      "next": LinkedListNode {
+        "data": 3,
+        "next": LinkedListNode {
+          "data": 5,
+          "next": LinkedListNode {
+            "data": 3,
+            "next": null,
+          },
+        },
+      },
+    },
+  },
+}
+`);
+  });
+});
---
+++ src/dailyInterview/partitionList.ts
@@ -0,0 +1,34 @@
+/*
+Hi, here's your problem today. This problem was recently asked by LinkedIn:
+
+Given a list of numbers and an integer k, partition/sort the list such that
+the all numbers less than k occur before k, and all numbers greater than k occur after the number k.
+*/
+
+export class LinkedListNode {
+  constructor(public data?: number, public next?: LinkedListNode) {}
+}
+
+export function partitionList(head: LinkedListNode, x: number): LinkedListNode {
+  let frontHead = new LinkedListNode();
+  let backHead = new LinkedListNode();
+  let front = frontHead;
+  let back = backHead;
+  let curr = head;
+
+  while (curr) {
+    if (curr.data < x) {
+      front.next = curr;
+      front = front.next;
+    } else {
+      back.next = curr;
+      back = back.next;
+    }
+    curr = curr.next;
+  }
+
+  front.next = backHead.next;
+  back.next = null;
+
+  return frontHead.next;
+}
