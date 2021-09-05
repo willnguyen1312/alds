b29244f81704cfd8d55fd789607f7067a5724197
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-05 21:17:34 +0700

    Solve removeAdjacentDuplicates 🎉

---
+++ src/dailyInterview/removeAdjacentDuplicates.test.ts
@@ -0,0 +1,25 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given a string, we want to remove 2 adjacent characters that are the same, and repeat the process with the new string until we can no longer perform the operation.
+
+Here's an example and some starter code:
+
+def remove_adjacent_dup(s):
+  # Fill this in.
+
+print(remove_adjacent_dup("cabba"))
+# Start with cabba
+# After remove bb: caa
+# After remove aa: c
+# print c
+*/
+
+import { removeAdjacentDuplicates } from './removeAdjacentDuplicates';
+
+describe('removeAdjacentDuplicates function', () => {
+  it('should work', () => {
+    const actual = removeAdjacentDuplicates('cabbaba');
+    expect(actual).toBe('cba');
+  });
+});
---
+++ src/dailyInterview/removeAdjacentDuplicates.ts
@@ -0,0 +1,30 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given a string, we want to remove 2 adjacent characters that are the same,
+and repeat the process with the new string until we can no longer perform the operation.
+*/
+
+export function removeAdjacentDuplicates(str: string): string {
+  const strArr = str.split('');
+  let index = 0;
+
+  while (index < strArr.length) {
+    let jndex = index;
+
+    while (jndex < strArr.length && strArr[index] === strArr[jndex + 1]) {
+      jndex++;
+    }
+
+    const range = jndex - index;
+
+    if (range > 0) {
+      strArr.splice(index, range + 1);
+      index--;
+    } else {
+      index++;
+    }
+  }
+
+  return strArr.join('');
+}
