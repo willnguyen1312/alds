d30deddd00c55962f91b8115c7af7b26292ce9d1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-05 10:22:17 +0700

    Solve countWaysOfMakingChange 🎉

---
+++ src/dailyInterview/countWaysOfMakingChange.test.ts
@@ -0,0 +1,7 @@
+import { countWaysOfMakingChange } from './countWaysOfMakingChange';
+
+describe('countWaysOfMakingChange function', () => {
+  it('should work', () => {
+    expect(countWaysOfMakingChange([1, 2, 3], 4)).toBe(4);
+  });
+});
