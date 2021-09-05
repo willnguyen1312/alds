a275468ba7f96b4bc63fdbba203f057af27c4272
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-05 16:56:22 +0700

    Solve connectedComponents 🎉

---
+++ src/dailyInterview/connectedComponents.test.ts
@@ -0,0 +1,14 @@
+import { connectedComponents } from './connectedComponents';
+
+describe('connectedComponents function', () => {
+  it('should work', () => {
+    const actual = connectedComponents([
+      [1, 2],
+      [2, 3],
+      [4, 1],
+      [5, 6],
+    ]);
+
+    expect(actual).toBe(2);
+  });
+});
---
+++ src/dailyInterview/connectedComponents.ts
@@ -0,0 +1,43 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given a list of undirected edges which represents a graph,
+find out the number of connected components.
+*/
+
+export function connectedComponents(connections: number[][]): number {
+  let result = 0;
+  const adj: Record<number, number[]> = {};
+  const visited: Record<number, boolean> = {};
+
+  for (const connection of connections) {
+    const [start, end] = connection;
+    adj[start] = adj[start] || [];
+    adj[end] = adj[end] || [];
+
+    adj[start].push(end);
+    adj[end].push(start);
+  }
+
+  for (let index = 0; index < connections.length; index++) {
+    const [start] = connections[index];
+
+    if (!visited[start]) {
+      result++;
+      dfs(start);
+    }
+  }
+
+  function dfs(connectionNode: number) {
+    if (visited[connectionNode]) {
+      return;
+    }
+
+    visited[connectionNode] = true;
+    for (let i = 0; i < adj[connectionNode].length; i++) {
+      dfs(adj[connectionNode][i]);
+    }
+  }
+
+  return result;
+}
