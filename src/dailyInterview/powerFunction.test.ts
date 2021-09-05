9487898000cad21e43044e118f56765fb726851e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-05 14:16:17 +0700

    Solve powerFunction 🎉

---
+++ src/dailyInterview/powerFunction.test.ts
@@ -0,0 +1,7 @@
+import { powerFunction } from './powerFunction';
+
+describe('powerFunction function', () => {
+  it('should work', () => {
+    expect(powerFunction(5, 3)).toBe(125);
+  });
+});
---
+++ src/dailyInterview/powerFunction.ts
@@ -0,0 +1,18 @@
+/*
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+The power function calculates x raised to the nth power.
+If implemented in O(n) it would simply be a for loop over n and multiply x n times. 
+Instead implement this power function in O(log n) time.
+You can assume that n will be a non-negative integer.
+*/
+
+export function powerFunction(x: number, y: number): number {
+  if (y === 0) return 1;
+
+  const temp = powerFunction(x, Math.floor(y / 2));
+
+  if (y % 2 === 0) return temp * temp;
+
+  return x * temp * temp;
+}
