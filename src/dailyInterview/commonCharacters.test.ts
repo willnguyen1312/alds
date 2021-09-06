8b5143f589a933bf98a6df73302c020228cf2381
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-06 15:53:52 +0700

    Solve commonCharacters 🎉

---
+++ src/dailyInterview/commonCharacters.test.ts
@@ -0,0 +1,8 @@
+import { commonCharacters } from './commonCharacters';
+
+describe('commonCharacters function', () => {
+  it('should work', () => {
+    const actual = commonCharacters(['google', 'facebook', 'youtube']);
+    expect(actual).toEqual(['e', 'o']);
+  });
+});
