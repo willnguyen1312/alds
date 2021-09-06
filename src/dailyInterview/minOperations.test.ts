078556aec9ffb6087f8828b932dbc15ef91757e9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-06 18:34:04 +0700

    Solve minOperations 🎉

---
+++ src/dailyInterview/minOperations.test.ts
@@ -0,0 +1,11 @@
+import { minOperations, minOperations2 } from './minOperations';
+
+describe('minOperation function', () => {
+  it('should work', () => {
+    const actual1 = minOperations2(5);
+    expect(actual1).toBe(3);
+
+    const actual2 = minOperations(6, 20);
+    expect(actual2).toBe(3);
+  });
+});
