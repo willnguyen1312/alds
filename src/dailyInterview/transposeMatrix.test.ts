bfbd9e01ed63093543f4cfea45c1c1541f397b90
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-06 17:34:00 +0700

    Solve transposeMatrix 🎉

---
+++ src/dailyInterview/transposeMatrix.test.ts
@@ -0,0 +1,16 @@
+import { transposeMatrix } from './transposeMatrix';
+
+describe('transposeMatrix function', () => {
+  it('should work', () => {
+    const actual = transposeMatrix([
+      [1, 2, 3],
+      [4, 5, 6],
+    ]);
+
+    expect(actual).toEqual([
+      [1, 4],
+      [2, 5],
+      [3, 6],
+    ]);
+  });
+});
---
+++ src/dailyInterview/transposeMatrix.ts
@@ -0,0 +1,30 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Twitter:
+
+Given a matrix, transpose it. Transposing a matrix means the rows are now the column and vice-versa.
+*/
+
+export function transposeSquareMatrix(matrix: number[][]) {
+  const length = matrix.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+}
+
+export function transposeMatrix(arr: number[][]): number[][] {
+  const row = arr.length;
+  const column = arr[0].length;
+  const result: number[][] = Array.from({ length: column }, () =>
+    Array.from({ length: row })
+  );
+
+  for (let i = 0; i < column; i++) {
+    for (let j = 0; j < row; j++) {
+      result[i][j] = arr[j][i];
+    }
+  }
+
+  return result;
+}
