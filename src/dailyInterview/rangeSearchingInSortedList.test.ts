c64bd264fa40115220ce28d6cb3675f00c7fc9b5
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-07 16:06:28 +0700

    Solve rangeSearchingInSortedList 🎉

---
+++ src/dailyInterview/rangeSearchingInSortedList.test.ts
@@ -0,0 +1,10 @@
+import { rangeSearchingInSortedList } from './rangeSearchingInSortedList';
+
+describe('rangeSearchingInSortedList function', () => {
+  it('should work', () => {
+    const actual1 = rangeSearchingInSortedList([1, 1, 3, 5, 7], 1);
+    expect(actual1).toEqual([0, 1]);
+    const actual2 = rangeSearchingInSortedList([1, 2, 3, 4], 5);
+    expect(actual2).toEqual([-1, -1]);
+  });
+});
---
+++ src/dailyInterview/rangeSearchingInSortedList.ts
@@ -0,0 +1,32 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Twitter:
+
+Given a sorted list with duplicates, and a target number n,
+find the range in which the number exists (represented as a tuple (low, high), both inclusive.
+If the number does not exist in the list, return (-1, -1)).
+*/
+
+export function rangeSearchingInSortedList(
+  numbs: number[],
+  target: number
+): number[] {
+  let start = -1;
+  let end = -1;
+
+  while (start < numbs.length) {
+    const found = target === numbs[start];
+
+    if (found) {
+      end = start + 1;
+      while (end < numbs.length && numbs[start] === numbs[end]) {
+        end++;
+      }
+
+      return [start, end - 1];
+    }
+
+    start++;
+  }
+
+  return [-1, -1];
+}
