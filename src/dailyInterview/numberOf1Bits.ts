7907fd9772385932f39e263586b31303a6d3017e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-07 16:46:56 +0700

    Solve numberOf1Bits 🎉

---
+++ src/dailyInterview/numberOf1Bits.ts
@@ -0,0 +1,16 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Google:
+
+Given an integer, find the number of 1 bits it has.
+*/
+
+export function numberOf1Bits(numb: number): number {
+  const numbResult: number[] = [];
+
+  while (numb > 0) {
+    numbResult.push(numb % 2);
+    numb = Math.floor(numb / 2);
+  }
+
+  return numbResult.filter((numb) => numb === 1).length;
+}
