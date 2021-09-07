6f3ec9d366c2ddb3b443dceb973f93811f27ab37
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-07 19:30:06 +0700

    Solve findKLargestNumber 🎉

---
+++ src/dailyInterview/findKLargestNumber.test.ts
@@ -0,0 +1,8 @@
+import { findKLargestNumber } from './findKLargestNumber';
+
+describe('findKLargest', () => {
+  it('should work for the first case', () => {
+    expect(findKLargestNumber([1, 4, 3, 2, 5, 6, 7], 2)).toBe(6);
+    expect(findKLargestNumber([3, 5, 2, 4, 6, 8], 3)).toBe(5);
+  });
+});
---
+++ src/dailyInterview/findKLargestNumber.ts
@@ -0,0 +1,46 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Twitter:
+
+Find the k-th largest number in a sequence of unsorted numbers.
+*/
+
+function getKth(k: number, nums: number[], start: number, end: number): number {
+  let pivot = nums[end];
+
+  let left = start;
+  let right = end;
+
+  while (true) {
+    while (nums[left] < pivot && left < right) {
+      left++;
+    }
+
+    while (nums[right] >= pivot && right > left) {
+      right--;
+    }
+
+    if (left === right) {
+      break;
+    }
+
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+  }
+
+  [nums[end], nums[left]] = [nums[left], nums[end]];
+
+  if (k === left + 1) {
+    return pivot;
+  } else if (k < left + 1) {
+    return getKth(k, nums, start, left - 1);
+  } else {
+    return getKth(k, nums, left + 1, end);
+  }
+}
+
+export function findKLargestNumber(nums: number[], k: number) {
+  if (k < 1 || nums === null) {
+    return 0;
+  }
+
+  return getKth(nums.length - k + 1, nums, 0, nums.length - 1);
+}
