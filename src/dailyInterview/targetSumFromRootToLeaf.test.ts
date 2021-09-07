03266858a6b50d0d36d51512f92a687c50b1089d
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-07 16:26:28 +0700

    Solve targetSumFromRootToLeaf 🎉

---
+++ src/dailyInterview/targetSumFromRootToLeaf.test.ts
@@ -0,0 +1,17 @@
+import {
+  BinaryTreeNode,
+  targetSumFromRootToLeaf,
+} from './targetSumFromRootToLeaf';
+
+describe('targetSumFromRootToLeaf function', () => {
+  it('should work', () => {
+    const n6 = new BinaryTreeNode(6);
+    const n4 = new BinaryTreeNode(4);
+    const n3 = new BinaryTreeNode(3, null, n4);
+    const n2 = new BinaryTreeNode(2, null, n6);
+    const n1 = new BinaryTreeNode(1, n2, n3);
+
+    const actual = targetSumFromRootToLeaf(n1, 9);
+    expect(actual).toBe(true);
+  });
+});
---
+++ src/dailyInterview/targetSumFromRootToLeaf.ts
@@ -0,0 +1,32 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Uber:
+
+Given a binary tree, and a target number, find if there is a path
+from the root to any leaf that sums up to the target.
+*/
+
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+export function targetSumFromRootToLeaf(
+  root: BinaryTreeNode,
+  target: number
+): boolean {
+  if (!root) {
+    return false;
+  }
+
+  if (root.data === target && !root.left && !root.right) {
+    return true;
+  }
+
+  return (
+    targetSumFromRootToLeaf(root.left, target - root.data) ||
+    targetSumFromRootToLeaf(root.right, target - root.data)
+  );
+}
