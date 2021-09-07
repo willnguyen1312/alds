1b96de7faa5560253568ecf55fb8f3ea30455fa5
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-07 17:15:51 +0700

    Solve maxNoneAdjacentList 🎉

---
+++ src/dailyInterview/maxNoneAdjacentList.test.ts
@@ -0,0 +1,8 @@
+import { maxNoneAdjacentList } from './maxNoneAdjacentList';
+
+describe('maxNoneAdjacentList function', () => {
+  it('should work', () => {
+    const actual = maxNoneAdjacentList([3, 4, 1, 1]);
+    expect(actual).toBe(5);
+  });
+});
---
+++ src/dailyInterview/maxNoneAdjacentList.ts
@@ -0,0 +1,21 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Google:
+
+Given a list of positive numbers, find the largest possible set such that
+no elements are adjacent numbers of each other.
+*/
+
+export function maxNoneAdjacentList(numbs: number[]): number {
+  const n = numbs.length;
+  let incl = numbs[0];
+  let excl = 0;
+  let exclNew: number;
+
+  for (let i = 1; i < n; i++) {
+    exclNew = Math.max(incl, excl);
+    incl = excl + numbs[i];
+    excl = exclNew;
+  }
+
+  return Math.max(incl, excl);
+}
