759407ae4fdaf325d0aa9690794d731123c616b0
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-11 13:12:06 +0700

    Solve countWaysOfDecode 🎉

---
+++ src/dailyCoding/countWaysOfDecode.test.ts
@@ -0,0 +1,8 @@
+import { countWaysOfDecode } from './countWaysOfDecode';
+
+describe('countWaysOfDecode function', () => {
+  it('should work', () => {
+    const actual = countWaysOfDecode('111');
+    expect(actual).toBe(3);
+  });
+});
