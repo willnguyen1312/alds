0242f7e418ae740e9ef7e8e8a81289df8cfa9b5a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-12 12:08:49 +0700

    Solve minPathSum 🎉

---
+++ src/al/minPathSum.ts
@@ -0,0 +1,12 @@
+export function minPathSum(grid: number[][]): number {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (i == 0 && j == 0) continue;
+
+      const up = i - 1 >= 0 ? grid[i - 1][j] : Infinity;
+      const left = j - 1 >= 0 ? grid[i][j - 1] : Infinity;
+      grid[i][j] = Math.min(grid[i][j] + up, grid[i][j] + left);
+    }
+  }
+  return grid[grid.length - 1][grid[0].length - 1];
+}
