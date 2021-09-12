49f908735eee3521aedd6b686afc62cacb70402d
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-12 11:58:42 +0700

    Solve minStepsFromStartToEnd 🎉

---
+++ src/dailyCoding/minStepsFromStartToEnd.test.ts
@@ -0,0 +1,15 @@
+import { minStepsFromStartToEnd } from './minStepsFromStartToEnd';
+
+describe('minStepsFromStartToEnd function', () => {
+  it('should work', () => {
+    const board = [
+      [false, false, false, false],
+      [true, true, false, true],
+      [false, false, false, false],
+      [false, false, false, false],
+    ];
+
+    const actual = minStepsFromStartToEnd(board, 3, 0, 0, 0);
+    expect(actual).toBe(7);
+  });
+});
