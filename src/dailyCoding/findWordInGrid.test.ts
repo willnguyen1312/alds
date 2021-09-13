cd7635b438199aad4ff8987efee0cbb1e5afd5c9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-13 17:03:55 +0700

    Solve findWordInGrid 🎉

---
+++ src/dailyCoding/findWordInGrid.test.ts
@@ -0,0 +1,18 @@
+import { findWordInGrid } from './findWordInGrid';
+
+describe('findWordInGrid function', () => {
+  it('should work', () => {
+    const grid = [
+      ['F', 'A', 'C', 'I'],
+      ['O', 'B', 'Q', 'P'],
+      ['A', 'N', 'O', 'B'],
+      ['M', 'A', 'S', 'S'],
+    ];
+
+    const actual1 = findWordInGrid(grid, 'FOAM');
+    expect(actual1).toBe(true);
+
+    const actual2 = findWordInGrid(grid, 'MASS');
+    expect(actual2).toBe(true);
+  });
+});
---
+++ src/dailyCoding/findWordInGrid.ts
@@ -0,0 +1,52 @@
+/*
+This problem was asked by Microsoft.
+
+Given a 2D matrix of characters and a target word, write a function that
+returns whether the word can be found in the matrix by going left-to-right, or up-to-down.
+
+For example, given the following matrix:
+
+[['F', 'A', 'C', 'I'],
+ ['O', 'B', 'Q', 'P'],
+ ['A', 'N', 'O', 'B'],
+ ['M', 'A', 'S', 'S']]
+ 
+and the target word 'FOAM', you should return true, since it's the leftmost column.
+Similarly, given the target word 'MASS', you should return true, since it's the last row.
+*/
+
+export function findWordInGrid(grid: string[][], target: string): boolean {
+  const maxRow = grid.length;
+  const maxColumn = grid[0].length;
+
+  function checkValid(row: number, column: number) {
+    let result = '';
+    for (let i = row; i < maxRow; i++) {
+      result += grid[i][column];
+      if (result === target) {
+        return true;
+      }
+    }
+
+    result = '';
+
+    for (let i = column; i < maxColumn; i++) {
+      result += grid[row][i];
+      if (result === target) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  for (let i = 0; i < maxRow; i++) {
+    for (let j = 0; j < maxColumn; j++) {
+      if (checkValid(i, j)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
