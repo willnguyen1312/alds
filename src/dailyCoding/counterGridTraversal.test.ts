a9c6c0520fe8d2b2c892a5aecfcae557d0ee956c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-13 18:26:46 +0700

    Solve counterGridTraversal 🎉

---
+++ src/dailyCoding/counterGridTraversal.test.ts
@@ -0,0 +1,16 @@
+import { counterGridTraversal } from './counterGridTraversal';
+
+describe('counterGridTraversal function', () => {
+  it('should work', () => {
+    expect(
+      counterGridTraversal([
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+      ])
+    ).toEqual([
+      1, 6, 11, 16, 17, 18, 19, 20, 15, 10, 5, 4, 3, 2, 7, 12, 13, 14, 9, 8,
+    ]);
+  });
+});
