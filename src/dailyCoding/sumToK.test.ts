18c7ce9692dff5269c8f8cb80d17770eafb4233b
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-13 14:52:43 +0700

    Solve sumToK 🎉

---
+++ src/dailyCoding/sumToK.test.ts
@@ -0,0 +1,8 @@
+import { sumToK } from './sumToK';
+
+describe('sumToK function', () => {
+  it('should work', () => {
+    const actual = sumToK([12, 1, 61, 5, 9, 2], 24);
+    expect(actual).toEqual([1, 2, 9, 12]);
+  });
+});
---
+++ src/dailyCoding/sumToK.ts
@@ -0,0 +1,38 @@
+/*
+Given a list of integers S and a target number k,
+write a function that returns a subset of S that adds up to k.
+If such a subset cannot be made, then return null.
+
+Integers can appear more than once in the list.
+You may assume all numbers in the list are positive.
+
+For example, given S = [12, 1, 61, 5, 9, 2] and k = 24,
+return [12, 9, 2, 1] since it sums up to 24.
+*/
+
+export function sumToK(candidates: number[], target: number): number[] {
+  const result: number[][] = [];
+  const path: number[] = [];
+
+  function sumToKHelper(sum: number, startIndex: number) {
+    if (sum === target) {
+      result.push([...path]);
+      return;
+    }
+
+    for (
+      let i = startIndex;
+      i < candidates.length && candidates[i] + sum <= target;
+      i++
+    ) {
+      path.push(candidates[i]);
+      sumToKHelper(sum + candidates[i], i + 1);
+      path.pop();
+    }
+  }
+
+  candidates.sort((a, b) => a - b);
+  sumToKHelper(0, 0);
+
+  return result[0];
+}
