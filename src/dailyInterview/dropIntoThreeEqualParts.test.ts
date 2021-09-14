094d7090771ee3ef3c0e49ebe125430e197aded5
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-14 21:01:32 +0700

    Solve dropIntoThreeEqualParts 🎉

--- src/dailyInterview/dropIntoThreeEqualParts.test.ts
+++ src/dailyInterview/dropIntoThreeEqualParts.test.ts
@@ -3,6 +3,6 @@ import { dropIntoThreeEqualParts } from './dropIntoThreeEqualParts';
 describe('dropIntoThreeEqualParts function', () => {
   it('should work', () => {
     const actual = dropIntoThreeEqualParts([2, 4, 5, 3, 3, 9, 2, 2, 2]);
-    // console.log(actual);
+    expect(actual).toBe(true);
   });
 });
