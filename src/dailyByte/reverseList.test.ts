2f3cfd04bd00c17e28fed5523575d733bfc55cf3
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-15 17:06:51 +0700

    Solve reverseList 🎉

---
+++ src/dailyByte/reverseList.test.ts
@@ -0,0 +1,24 @@
+import { LinkedListNode, reverseList } from './reverseList';
+
+describe('reverseList function', () => {
+  it('should work', () => {
+    const node = new LinkedListNode(
+      1,
+      new LinkedListNode(2, new LinkedListNode(3))
+    );
+
+    const actual = reverseList(node);
+    expect(actual).toMatchInlineSnapshot(`
+LinkedListNode {
+  "data": 3,
+  "next": LinkedListNode {
+    "data": 2,
+    "next": LinkedListNode {
+      "data": 1,
+      "next": null,
+    },
+  },
+}
+`);
+  });
+});
---
+++ src/dailyByte/reverseList.ts
@@ -0,0 +1,31 @@
+/*
+This question is asked by Facebook. Given a linked list, containing unique values, reverse it,
+and return the result. 
+
+Ex: Given the following linked lists...
+
+1->2->3->null, return a reference to the node that contains 3 which
+points to a list that looks like the following: 3->2->1->null
+7->15->9->2->null, return a reference to the node that contains 2 which
+points to a list that looks like the following: 2->9->15->7->null
+1->null, return a reference to the node that contains 1 which points to a list that
+looks like the following: 1->null
+*/
+
+export class LinkedListNode {
+  constructor(public data: number, public next?: LinkedListNode) {}
+}
+
+export function reverseList(head: LinkedListNode) {
+  function reverseListHelper(head: LinkedListNode, tail: LinkedListNode) {
+    if (!head) {
+      return tail;
+    }
+    const next = head.next;
+    head.next = tail;
+
+    return reverseListHelper(next, head);
+  }
+
+  return reverseListHelper(head, null);
+}
