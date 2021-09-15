31f64f584527d89e63d61d4fe6e6e9d606ed7a6a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-15 17:52:59 +0700

    Solve lowestCommonAncestor 🎉

---
+++ src/dailyByte/lowestCommonAncestor.test.ts
@@ -0,0 +1,28 @@
+import { BinaryTreeNode, lowestCommonAncestor } from './lowestCommonAncestor';
+
+describe('lowestCommonAncestor function', () => {
+  it('should work', () => {
+    const node = new BinaryTreeNode(7);
+    node.left = new BinaryTreeNode(2);
+    node.left.left = new BinaryTreeNode(1);
+    node.left.right = new BinaryTreeNode(5);
+    node.right = new BinaryTreeNode(9);
+
+    const actual = lowestCommonAncestor(node, node.left.left, node.left);
+    expect(actual).toMatchInlineSnapshot(`
+BinaryTreeNode {
+  "data": 2,
+  "left": BinaryTreeNode {
+    "data": 1,
+    "left": undefined,
+    "right": undefined,
+  },
+  "right": BinaryTreeNode {
+    "data": 5,
+    "left": undefined,
+    "right": undefined,
+  },
+}
+`);
+  });
+});
---
+++ src/dailyByte/lowestCommonAncestor.ts
@@ -0,0 +1,59 @@
+/*
+Given a binary search tree that contains unique values and two nodes within the tree, a, and b,
+return their lowest common ancestor. 
+Note: the lowest common ancestor of two nodes is the deepest node within the tree such that
+both nodes are descendants of it.
+
+Ex: Given the following tree...
+       7
+      / \
+    2    9
+   / \ 
+  1   5 
+and a = 1, b = 9, return a reference to the node containing 7.
+Ex: Given the following tree...
+
+        8
+       / \
+      3   9
+     / \ 
+    2   6
+and a = 2, b = 6, return a reference to the node containing 3.
+Ex: Given the following tree...
+
+        8
+       / \
+      6   9
+and a = 6, b = 8, return a reference to the node containing 8
+*/
+
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+export function lowestCommonAncestor(
+  root: BinaryTreeNode,
+  first: BinaryTreeNode,
+  second: BinaryTreeNode
+): BinaryTreeNode {
+  if (!root) {
+    return root;
+  }
+
+  if (root === first || root === second) {
+    return root;
+  }
+
+  const left = lowestCommonAncestor(root.left, first, second);
+  const right = lowestCommonAncestor(root.right, first, second);
+
+  if (left && right) {
+    return root;
+  }
+
+  return left || right;
+}
