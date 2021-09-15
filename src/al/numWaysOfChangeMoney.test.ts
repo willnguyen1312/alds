28b6b4333dfe6f9dd9c15ad00b8afdd99d98b45c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-15 16:15:55 +0700

    Solve numWaysOfChangeMoney 🎉

---
+++ src/al/numWaysOfChangeMoney.test.ts
@@ -0,0 +1,8 @@
+import { numWaysOfChangeMoney } from './numWaysOfChangeMoney';
+
+describe('numWaysOfChangeMoney fucntion', () => {
+  it('should work', () => {
+    const actual = numWaysOfChangeMoney([1, 2, 3], 4);
+    expect(actual).toBe(4);
+  });
+});
---
+++ src/al/numWaysOfChangeMoney.ts
@@ -0,0 +1,13 @@
+export function numWaysOfChangeMoney(coins: number[], target: number): number {
+  const table = Array(target + 1).fill(0);
+
+  table[0] = 1;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = coins[i]; j < table.length; j++) {
+      table[j] += table[j - coins[i]];
+    }
+  }
+
+  return table[target];
+}
