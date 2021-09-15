58e6ae0a5a10daa23b0c7a0c4af279955642804f
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-15 16:39:18 +0700

    Solve removeLinkedListNode 🎉

---
+++ src/dailyByte/removeLinkedListNode.test.ts
@@ -0,0 +1,21 @@
+import { LinkedListNode, removeLinkedListNode } from './removeLinkedListNode';
+
+describe('removeLinkedListNode function', () => {
+  it('should work', () => {
+    const node = new LinkedListNode(
+      1,
+      new LinkedListNode(2, new LinkedListNode(3))
+    );
+
+    const actual = removeLinkedListNode(node, 3);
+    expect(actual).toMatchInlineSnapshot(`
+LinkedListNode {
+  "data": 1,
+  "next": LinkedListNode {
+    "data": 2,
+    "next": undefined,
+  },
+}
+`);
+  });
+});
