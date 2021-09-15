44dadaabb610f31dffc1e9449ac7a36762b7910c
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-15 17:26:25 +0700

    Solve greaterElement 🎉

---
+++ src/dailyByte/greaterElement.test.ts
@@ -0,0 +1,8 @@
+import { greaterElement } from './greaterElement';
+
+describe('greaterElement function', () => {
+  it('should work', () => {
+    const actual = greaterElement([4, 1, 2], [1, 3, 4, 2]);
+    expect(actual).toEqual([-1, 3, -1]);
+  });
+});
