b5e6460c299c36e49badec7c51c2015a245bba57
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-15 17:14:14 +0700

    Solve compareKeyStrokes 🎉

---
+++ src/dailyByte/compareKeyStrokes.test.ts
@@ -0,0 +1,8 @@
+import { compareKeyStrokes } from './compareKeyStrokes';
+
+describe('compareKeyStrokes function', () => {
+  it('should work', () => {
+    const actual = compareKeyStrokes('ABC#', 'CD##AB');
+    expect(actual).toBe(true);
+  });
+});
---
+++ src/dailyByte/compareKeyStrokes.ts
@@ -0,0 +1,29 @@
+/*
+This question is asked by Amazon. Given two strings s and t
+ which represents a sequence of keystrokes, where # denotes a backspace,
+ return whether or not the sequences produce the same result. 
+
+Ex: Given the following strings...
+
+s = "ABC#", t = "CD##AB", return true
+s = "como#pur#ter", t = "computer", return true
+s = "cof#dim#ng", t = "code", return false
+*/
+
+export function compareKeyStrokes(first: string, second: string): boolean {
+  function processSequence(str: string): string {
+    const stack: string[] = [];
+
+    for (const char of str) {
+      if (char === '#') {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    }
+
+    return stack.join('');
+  }
+
+  return processSequence(first) === processSequence(second);
+}
