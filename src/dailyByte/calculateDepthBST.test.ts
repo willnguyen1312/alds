005f57160a8abe0a9d1509f3f172977196be486a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-16 12:57:08 +0700

    Solve calculateDepthBST 🎉

---
+++ src/dailyByte/calculateDepthBST.test.ts
@@ -0,0 +1,13 @@
+import { BinaryTreeNode, calculateDepthBST } from './calculateDepthBST';
+
+describe('calculateDepthBST function', () => {
+  it('should work', () => {
+    const node = new BinaryTreeNode(5);
+    node.left = new BinaryTreeNode(1);
+    node.right = new BinaryTreeNode(29);
+    node.right.left = new BinaryTreeNode(4);
+    node.right.right = new BinaryTreeNode(13);
+
+    expect(calculateDepthBST(node)).toBe(3);
+  });
+});
---
+++ src/dailyByte/calculateDepthBST.ts
@@ -0,0 +1,38 @@
+/*
+Given a binary tree, return its maximum depth. 
+Note: the maximum depth is defined as the number of nodes
+along the longest path from root node to leaf node. 
+
+Ex: Given the following tree…
+
+    9
+   / \
+  1   2
+return 2
+Ex: Given the following tree…
+
+    5
+   / \
+  1  29
+    /  \
+   4   13
+return 3
+*/
+
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+export function calculateDepthBST(root: BinaryTreeNode) {
+  if (!root) {
+    return 0;
+  }
+
+  return (
+    Math.max(calculateDepthBST(root.left), calculateDepthBST(root.right)) + 1
+  );
+}
