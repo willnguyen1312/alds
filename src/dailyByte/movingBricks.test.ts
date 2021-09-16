efb5f9446c2a4915a5deb3efd9da28de5bc39810
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-16 16:26:15 +0700

    Solve movingBricks 🎉

---
+++ src/dailyByte/movingBricks.test.ts
@@ -0,0 +1,8 @@
+import { movingBricks } from './movingBricks';
+
+describe('movingBricks function', () => {
+  it('should work', () => {
+    const actual = movingBricks([1000, 1000, 1000, 2000], 5000);
+    expect(actual).toBe(3);
+  });
+});
