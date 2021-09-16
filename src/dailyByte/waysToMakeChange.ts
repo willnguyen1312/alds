3134de692747254b2e0d24e6a2f8d03f5d04facf
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-16 15:34:29 +0700

    Solve waysToMakeChange 🎉

---
+++ src/dailyByte/waysToMakeChange.ts
@@ -0,0 +1,15 @@
+export function waysToMakeChange(n: number, denoms: number[]): number {
+  const ways = new Array(n + 1).fill(0);
+
+  ways[0] = 1;
+
+  for (let denom of denoms) {
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount) {
+        ways[amount] += ways[amount - denom];
+      }
+    }
+  }
+
+  return ways[n];
+}
