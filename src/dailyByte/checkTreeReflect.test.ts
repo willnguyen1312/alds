f46127dc0267e8cfb2f447aa5dd3280d92bcb02a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-16 13:37:52 +0700

    Solve checkTreeReflect 🎉

---
+++ src/dailyByte/checkTreeReflect.test.ts
@@ -0,0 +1,14 @@
+import { BinaryTreeNode, checkTreeReflect } from './checkTreeReflect';
+
+describe('checkTreeReflect function', () => {
+  it('should work', () => {
+    const node = new BinaryTreeNode(2);
+    node.left = new BinaryTreeNode(5);
+    node.left.right = new BinaryTreeNode(7);
+    node.right = new BinaryTreeNode(5);
+    node.right.right = new BinaryTreeNode(7);
+
+    const actual = checkTreeReflect(node);
+    expect(actual).toBe(false);
+  });
+});
---
+++ src/dailyByte/checkTreeReflect.ts
@@ -0,0 +1,77 @@
+/*
+Given a binary tree, return whether or not it forms a reflection across its center
+(i.e. a line drawn straight down starting from the root). 
+Note: a reflection is when an image,
+flipped across a specified line, forms the same image. 
+
+Ex: Given the following tree…
+
+   2
+ /   \
+1     1
+return true as when the tree is reflected across its center all the nodes match.
+Ex: Given the following tree…
+
+    1
+   / \
+  5   5
+   \    \
+    7    7
+return false as when the tree is reflected
+across its center the nodes containing sevens do not match.
+*/
+
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+function checkValid(numbs: (number | undefined)[]): boolean {
+  if (numbs.length % 2) {
+    return false;
+  }
+
+  const mid = Math.floor(numbs.length / 2);
+
+  for (let index = 0; index < mid; index++) {
+    const element = numbs[index];
+    const compared = numbs[numbs.length - 1 - index];
+    if (element !== compared) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function checkTreeReflect(root: BinaryTreeNode): boolean {
+  let queue: BinaryTreeNode[] = [root];
+
+  while (queue.length) {
+    const toQueue: BinaryTreeNode[] = [];
+    const numbs: number[] = [];
+
+    for (const item of queue) {
+      numbs.push(item.left?.data);
+      if (item.left) {
+        toQueue.push(item.left);
+      }
+
+      numbs.push(item.right?.data);
+      if (item.right) {
+        toQueue.push(item.right);
+      }
+    }
+
+    if (!checkValid(numbs)) {
+      return false;
+    }
+
+    queue = toQueue;
+  }
+
+  return true;
+}
