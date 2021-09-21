89a0da7d3abfc8fe800f1661e66ab1f1e799635b
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-21 15:05:22 +0700

    Solve characterScramble 🎉

---
+++ src/dailyCoding/characterScramble.test.ts
@@ -0,0 +1,11 @@
+import { characterScramble } from './characterScramble';
+
+describe('characterScramble function', () => {
+  it('should work', () => {
+    const actual = characterScramble('bat', 'cat');
+    expect(actual).toBe(false);
+
+    const actual1 = characterScramble('dog', 'didnotgo');
+    expect(actual1).toBe(true);
+  });
+});
---
+++ src/dailyCoding/characterScramble.ts
@@ -0,0 +1,23 @@
+/*
+This question is asked by Amazon.
+
+Given two strings, passage and text return whether or not the characters
+in text can be used to form the given passage. 
+
+Note: Each character in text may only be used once and passage
+and text will only contain lowercase alphabetical characters. 
+
+Ex: Given the following passage and text…
+
+passage = "bat", text = "cat", return false.
+
+Ex: Given the following passage and text…
+passage = "dog" text = "didnotgo", return true.
+*/
+
+export function characterScramble(passage: string, text: string): boolean {
+  const passageArr: string[] = passage.split('');
+  const textCharSet: Set<string> = new Set(text);
+
+  return passageArr.every((char) => textCharSet.has(char));
+}
