61282a891bf8cce9936cc16fb771964affd9e9bf
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-21 14:56:39 +0700

    Solve noneDecreasingByModOne 🎉

---
+++ src/al/noneDecreasingByModOne.test.ts
@@ -0,0 +1,11 @@
+import { noneDecreasingByModOne } from './noneDecreasingByModOne';
+
+describe('noneDecreasingByModOne function', () => {
+  it('should work', () => {
+    const actual1 = noneDecreasingByModOne([10, 5, 7]);
+    expect(actual1).toBe(true);
+
+    const actual2 = noneDecreasingByModOne([10, 5, 1]);
+    expect(actual2).toBe(false);
+  });
+});
---
+++ src/al/noneDecreasingByModOne.ts
@@ -0,0 +1,31 @@
+/*
+This problem was asked by Facebook.
+
+Given an array of integers, write a function to determine whether
+the array could become non-decreasing by modifying at most 1 element.
+
+For example, given the array [10, 5, 7], you should return true,
+since we can modify the 10 into a 1 to make the array non-decreasing.
+
+Given the array [10, 5, 1], you should return false,
+since we can't modify any one element to get a non-decreasing array.
+*/
+
+export function noneDecreasingByModOne(numbs: number[]): boolean {
+  let count = 0;
+
+  for (let index = 0; index < numbs.length - 1; index++) {
+    const currentElement = numbs[index];
+    const nextElement = numbs[index + 1];
+
+    if (nextElement < currentElement) {
+      count++;
+    }
+
+    if (count > 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
