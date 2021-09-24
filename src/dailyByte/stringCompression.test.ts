2810e7abd41c3eb0d4a7c92d87da298fa7a8e7a7
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-24 18:03:49 +0700

    Solve stringCompression 🎉

---
+++ src/dailyByte/stringCompression.test.ts
@@ -0,0 +1,14 @@
+import { stringCompression } from './stringCompression';
+
+describe('stringCompression function', () => {
+  it('should work', () => {
+    const actual1 = stringCompression(['a', 'a', 'a', 'a', 'a', 'a']);
+    expect(actual1).toBe(2);
+
+    const actual2 = stringCompression(['a', 'a', 'b', 'b', 'c', 'c']);
+    expect(actual2).toBe(6);
+
+    const actual3 = stringCompression(['a', 'b', 'c']);
+    expect(actual3).toBe(6);
+  });
+});
---
+++ src/dailyByte/stringCompression.ts
@@ -0,0 +1,40 @@
+/*
+This question is asked by Facebook.
+
+Given a character array, compress it in place and return the new length of the array. 
+Note: You should only compress the array if its compressed form
+will be at least as short as the length of its original form. 
+
+Ex: Given the following character array chars…
+
+chars = ['a', 'a', 'a', 'a', 'a', 'a'], return 2.
+chars should be compressed to look like the following:
+chars = ['a', '6']
+Ex: Given the following character array chars…
+
+chars = ['a', 'a', 'b', 'b', 'c', 'c'], return 6.
+chars should be compressed to look like the following:
+chars = ['a', '2', 'b', '2', 'c', '2']
+Ex: Given the following character array chars…
+
+chars = ['a', 'b', 'c'], return 3.
+In this case we chose not to compress chars.
+*/
+
+export function stringCompression(strs: string[]): number {
+  const compressionResult: string[] = [];
+  let current = 0;
+
+  while (current < strs.length) {
+    let forward = current + 1;
+
+    while (forward < strs.length && strs[current] === strs[forward]) {
+      forward++;
+    }
+
+    compressionResult.push(strs[current], `${forward - current}`);
+    current = forward;
+  }
+
+  return compressionResult.length;
+}
