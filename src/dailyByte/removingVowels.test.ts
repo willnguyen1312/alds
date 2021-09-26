82c99b9d3cb82ee4f030cba86eb6ec1a3b3a8dea
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-26 09:38:30 +0700

    Solve removingVowels 🎉

---
+++ src/dailyByte/removingVowels.test.ts
@@ -0,0 +1,12 @@
+import { removingVowels } from './removingVowels';
+
+describe('removingVowels function', () => {
+  it('should work', () => {
+    const actual1 = removingVowels('aeiou');
+    expect(actual1).toBe('');
+    const actual2 = removingVowels('byte');
+    expect(actual2).toBe('byt');
+    const actual3 = removingVowels('xyz');
+    expect(actual3).toBe('xyz');
+  });
+});
---
+++ src/dailyByte/removingVowels.ts
@@ -0,0 +1,19 @@
+/*
+This question is asked by Amazon.
+Given a string s remove all the vowels it contains and return the resulting string. 
+Note: In this problem y is not considered a vowel. 
+
+Ex: Given the following string s…
+
+s = "aeiou", return ""
+Ex: Given the following string s…
+
+s = "byte", return "byt"
+Ex: Given the following string s…
+
+s = "xyz", return "xyz"
+*/
+
+export function removingVowels(str: string): string {
+  return str.replace(/[aeiou]/g, '');
+}
