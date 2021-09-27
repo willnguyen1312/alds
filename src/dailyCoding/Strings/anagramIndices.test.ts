15bdeb8c1b1ef70c19a392dd49a1eb1d164ce2f1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-27 14:38:29 +0700

    Solve anagramIndices 🎉

---
+++ src/dailyCoding/Strings/anagramIndices.test.ts
@@ -0,0 +1,8 @@
+import { anagramIndices } from './anagramIndices';
+
+describe('anagramIndices function', () => {
+  it('should work', () => {
+    const actual = anagramIndices('ab', 'abxaba');
+    expect(actual).toEqual([0, 3, 4]);
+  });
+});
