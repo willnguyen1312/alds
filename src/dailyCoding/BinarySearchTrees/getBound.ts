59e97801c57ad7fc3e00c4fbc104dcdc742a8ac9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-28 15:06:44 +0700

    Solve getBound 🎉

---
+++ src/dailyCoding/BinarySearchTrees/getBound.ts
@@ -0,0 +1,32 @@
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+export function getBound(
+  root: BinaryTreeNode,
+  target: number,
+  floor?: number,
+  ceil?: number
+) {
+  if (!root) {
+    return [floor, ceil];
+  }
+
+  if (target === root.data) {
+    return [target, target];
+  }
+
+  if (target < root.data) {
+    [floor, ceil] = getBound(root.left, target, floor, root.data);
+  }
+
+  if (target > root.data) {
+    [floor, ceil] = getBound(root.right, target, root.data, ceil);
+  }
+
+  return [floor, ceil];
+}
