ab5d0075fe11139109825209ac9f9fab9595de4a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-28 14:39:08 +0700

    Solve countUnivalSubTrees 🎉

---
+++ src/dailyCoding/Trees/countUnivalSubTrees.test.ts
@@ -0,0 +1,15 @@
+import { BinaryTreeNode, countUnivalSubTrees } from './countUnivalSubTrees';
+
+describe('countUnivalSubTrees function', () => {
+  it('should work', () => {
+    const a = new BinaryTreeNode(0);
+    a.left = new BinaryTreeNode(1);
+    a.right = new BinaryTreeNode(0);
+    a.right.left = new BinaryTreeNode(1);
+    a.right.right = new BinaryTreeNode(0);
+    a.right.left.left = new BinaryTreeNode(1);
+    a.right.left.right = new BinaryTreeNode(1);
+
+    expect(countUnivalSubTrees(a)).toBe(5);
+  });
+});
---
+++ src/dailyCoding/Trees/countUnivalSubTrees.ts
@@ -0,0 +1,35 @@
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+export function countUnivalSubTrees(root: BinaryTreeNode) {
+  function helper(node: BinaryTreeNode): [number, boolean] {
+    if (!node) {
+      return [0, true];
+    }
+
+    const [leftCount, isLeftUnival] = helper(node.left);
+    const [rightCount, isRightUnival] = helper(node.right);
+    const totalCount = leftCount + rightCount;
+
+    if (isLeftUnival && isRightUnival) {
+      if (node.left && node.data !== node.left.data) {
+        return [totalCount, false];
+      }
+
+      if (root.right && root.data !== root.right.data) {
+        return [totalCount, false];
+      }
+
+      return [totalCount + 1, true];
+    }
+
+    return [totalCount, false];
+  }
+
+  return helper(root)[0];
+}
