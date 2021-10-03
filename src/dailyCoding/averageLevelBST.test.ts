9b89580ca97ecf0e5c826d37e6e861dd8f45ab4b
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-03 11:25:41 +0700

    Solve averageLevelBST 🎉

---
+++ src/dailyCoding/averageLevelBST.test.ts
@@ -0,0 +1,14 @@
+import { BinaryTreeNode, averageLevelBST } from './averageLevelBST';
+
+describe('averageLevelBST function', () => {
+  it('should work', () => {
+    const node = new BinaryTreeNode(
+      1,
+      new BinaryTreeNode(6, new BinaryTreeNode(1), new BinaryTreeNode(5)),
+      new BinaryTreeNode(8)
+    );
+
+    const actual = averageLevelBST(node);
+    expect(actual).toEqual([1, 7, 3]);
+  });
+});
---
+++ src/dailyCoding/averageLevelBST.ts
@@ -0,0 +1,50 @@
+/*
+This question is asked by Facebook.
+Given a reference to the root of a binary tree,
+return a list containing the average value in each level of the tree. 
+
+Ex: Given the following binary tree…
+
+       1
+      / \
+    6    8
+   / \ 
+  1   5 
+return [1.0, 7.0, 3.0]
+*/
+
+export class BinaryTreeNode {
+  constructor(
+    public data: number,
+    public left?: BinaryTreeNode,
+    public right?: BinaryTreeNode
+  ) {}
+}
+
+export function averageLevelBST(root: BinaryTreeNode): number[] {
+  const result: number[] = [];
+  let queue: BinaryTreeNode[] = [root];
+
+  while (queue.length) {
+    const toQueue: BinaryTreeNode[] = [];
+
+    let total = 0;
+
+    for (const item of queue) {
+      total += item.data;
+
+      if (item.left) {
+        toQueue.push(item.left);
+      }
+
+      if (item.right) {
+        toQueue.push(item.right);
+      }
+    }
+
+    result.push(total / queue.length);
+    queue = toQueue;
+  }
+
+  return result;
+}
