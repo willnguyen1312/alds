685f08688cda47b28e9ddab610def6209ebde5fa
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-03 11:50:37 +0700

    Fix generateRandomInt 🎉

--- src/dailyInterview/generateRandomInt.ts
+++ src/dailyInterview/generateRandomInt.ts
@@ -8,7 +8,7 @@ write a function that randomly generates a number from 0 to n-1 that isn't in l 
 export function generateRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export function getRandomArbitrary(min: number, max: number) {
