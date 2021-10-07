2fefec55dd0df16d0fc1a2213a7da7629a2b8e37
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-07 18:37:43 +0700

    Solve reverseBits 🎉

---
+++ src/dailyCoding/reverseBits.test.ts
@@ -0,0 +1,11 @@
+import { reverseBits } from './reverseBits';
+
+describe('reverseBits function', () => {
+  it('should work', () => {
+    const actual1 = reverseBits(11);
+    expect(actual1).toBe(13);
+
+    const actual2 = reverseBits(10);
+    expect(actual2).toBe(5);
+  });
+});
---
+++ src/dailyCoding/reverseBits.ts
@@ -0,0 +1,19 @@
+/*
+This question is asked by Apple.
+
+Given a 32 bit signed integer, reverse it and return the result. 
+Note: You may assume that the reversed integer will always fit
+within the bounds of the integer data type. 
+
+Ex: Given the following integer num…
+
+num = 550, return 55
+Ex: Given the following integer num…
+
+num = -37, return -73
+*/
+
+export function reverseBits(numb: number): number {
+  const numbStr = numb.toString(2).split('').reverse().join('');
+  return parseInt(numbStr, 2);
+}
