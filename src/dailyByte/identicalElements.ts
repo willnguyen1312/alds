cbabc740207783da3656af47c129d04d3108e82e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-10 10:46:02 +0700

    Solve identicalElements 🎉

---
+++ src/dailyByte/identicalElements.ts
@@ -0,0 +1,32 @@
+/*
+This question is asked by Google.
+
+Given an array, nums, and an integer k,
+return whether or not two unique indices exist such that nums[i] = nums[j]
+and the two indices i and jj are at most k elements apart.
+Ex: Given the following array nums and value k…
+
+nums = [1, 2, 1], k = 1, return false.
+Ex: Given the following array nums and value k…
+
+nums = [2, 3, 2], k = 2, return true.
+*/
+
+export function identicalElements(numbs: number[], distance: number): boolean {
+  const hash: Record<string, number> = {};
+
+  for (let index = 0; index < numbs.length; index++) {
+    const element = numbs[index];
+
+    if (hash[element] !== undefined) {
+      const diff = index - hash[element];
+      if (diff <= distance) {
+        return true;
+      }
+    }
+
+    hash[element] = index;
+  }
+
+  return false;
+}
