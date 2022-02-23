49bada514452d6b4b8bb3b333e14d6fa6a0e3a17
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-02-23 09:01:04 +0700

    Solve fib 🎉

---
+++ src/dailyCoding/fib.ts
@@ -0,0 +1,22 @@
+// This problem was asked by Apple.
+
+// Implement the function fib(n), which returns the nth number in the Fibonacci sequence,
+// using only O(1) space.
+
+export function fib(n: number): number {
+  if (n <= 2) {
+    return 1;
+  }
+
+  let first = 1;
+  let second = 1;
+  let result = 0;
+
+  for (let index = 2; index < n; index++) {
+    result = first + second;
+    first = second;
+    second = result;
+  }
+
+  return second;
+}
