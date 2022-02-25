48ed789cc23a1098ac0612bafa98430787812cc7
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-02-25 08:02:48 +0700

    Solve evenDigitIntegers 🎉

---
+++ src/dailyByte/evenDigitIntegers.test.ts
@@ -0,0 +1,8 @@
+import { evenDigitIntegers } from './evenDigitIntegers';
+
+describe('evenDigitIntegers function', () => {
+  it('should work', () => {
+    expect(evenDigitIntegers([1, 12, 123])).toBe(1);
+    expect(evenDigitIntegers([1, 32, 3492, 23])).toBe(3);
+  });
+});
---
+++ src/dailyByte/evenDigitIntegers.ts
@@ -0,0 +1,19 @@
+// Given an integer array, nums, return the total number of integers within nums
+// that have an even number of digits.
+
+// Ex: Given the following nums…
+
+// nums = [1, 12, 123], return 1 (12 is the only integer with an even number of digits).
+// Ex: Given the following nums…
+
+// nums = [1, 32, 3492, 23], return 3.
+
+export function evenDigitIntegers(numbs: number[]): number {
+  let count = 0;
+  for (let i = 0; i < numbs.length; i++) {
+    if (numbs[i].toString().length % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+}
