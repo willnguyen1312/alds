7a6da3f03b10c57ca00e28e429c45a688cd96076
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-02-27 08:39:40 +0700

    🧹

--- src/al/binaryTreeIsSymmetric.ts
+++ src/al/binaryTreeIsSymmetric.ts
@@ -12,6 +12,17 @@
  * }
  */
 
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function isSymmetric(root: TreeNode | null): boolean {
   if (root === null) return true;
   return checkSymmetric(root.left, root.right);
