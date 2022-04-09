3d8f8306af7ef9ce17fa6785dc60a2b331072c3c
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-09 17:33:29 +0700

    Solve validParentheses 🎉

---
+++ src/algoMonster/validParentheses.test.ts
@@ -0,0 +1,8 @@
+import { validParentheses } from './validParentheses';
+
+describe('validParentheses function', () => {
+  it('should work', () => {
+    expect(validParentheses('()')).toBe(true);
+    expect(validParentheses('(}')).toBe(false);
+  });
+});
---
+++ src/algoMonster/validParentheses.ts
@@ -0,0 +1,27 @@
+// For this question we ask you to determine whether or not a string has valid parentheses.
+// A string has valid parentheses if each bracket is closed and opened in the same order
+// and has the same type.
+// Parentheses has 3 types (), {} and []
+
+export function validParentheses(input: string): boolean {
+  const lookup: Map<string, string> = new Map([
+    [')', '('],
+    ['}', '{'],
+    [']', '['],
+  ]);
+
+  const stack: string[] = [];
+
+  for (const char of input) {
+    if (lookup.has(char)) {
+      const last = stack.pop();
+      if (last !== lookup.get(char)) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+}
