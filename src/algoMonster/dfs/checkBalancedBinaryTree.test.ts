29da821cf16fbe0aa46fcceca2a428e0d360d323
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 09:56:46 +0700

    Solve checkBalancedBinaryTree 🎉

---
+++ src/algoMonster/dfs/checkBalancedBinaryTree.test.ts
@@ -0,0 +1,13 @@
+import { checkBalancedBinaryTree, TreeNode } from './checkBalancedBinaryTree';
+
+describe('checkBalancedBinaryTree function', () => {
+  it('should work', () => {
+    const root = new TreeNode(
+      1,
+      new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+      new TreeNode(5)
+    );
+
+    expect(checkBalancedBinaryTree(root)).toBe(true);
+  });
+});
