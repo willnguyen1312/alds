82c3a485ff8f9be360286e8be1d2df3b4235aefd
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 08:49:51 +0700

    Solve findFirstOccurrence 🎉

---
+++ src/algoMonster/binarySearch/findFirstOccurrence.ts
@@ -0,0 +1,24 @@
+// Given a sorted array of integers and a target integer,
+// find the first occurrence of the target and return its index.
+// Return -1 if the target is not in the array.
+
+export function findFirstOccurrence(arr: number[], target: number): number {
+  let left = 0;
+  let right = arr.length - 1;
+  let result = -1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      result = mid;
+      right = mid - 1;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return result;
+}
