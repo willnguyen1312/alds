ecdb0f46161919840a44d29527fee31fee53f1ee
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 17:18:53 +0700

    Solve tripletToZero 🎉

---
+++ src/algoMonster/twoPointers/tripletToZero.test.ts
@@ -0,0 +1,11 @@
+import { tripletToZero } from './tripletToZero';
+
+describe('tripletToZero function', () => {
+  it('should work', () => {
+    expect(tripletToZero([0, -1, 2, -3, 1])).toEqual([
+      [-3, 1, 2],
+      [-1, 0, 1],
+    ]);
+  });
+});
+``;
---
+++ src/algoMonster/twoPointers/tripletToZero.ts
@@ -0,0 +1,24 @@
+export function tripletToZero(arr: number[]): number[][] {
+  const n = arr.length;
+  const result: number[][] = [];
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < n - 1; i++) {
+    let l = i + 1;
+    let r = n - 1;
+    let x = arr[i];
+    while (l < r) {
+      if (x + arr[l] + arr[r] === 0) {
+        result.push([x, arr[l], arr[r]]);
+        l++;
+        r--;
+      } else if (x + arr[l] + arr[r] < 0) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+  }
+
+  return result;
+}
