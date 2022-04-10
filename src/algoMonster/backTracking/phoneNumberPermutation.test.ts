69465f0d18d5497d7bf4374b2ed4b4a429bb53f4
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 13:26:17 +0700

    Solve phoneNumberPermutation 🎉

---
+++ src/algoMonster/backTracking/phoneNumberPermutation.test.ts
@@ -0,0 +1,17 @@
+import { phoneNumberPermutation } from './phoneNumberPermutation';
+
+describe('phoneNumberPermutation function', () => {
+  it('should work', () => {
+    expect(phoneNumberPermutation('23')).toEqual([
+      'ad',
+      'ae',
+      'af',
+      'bd',
+      'be',
+      'bf',
+      'cd',
+      'ce',
+      'cf',
+    ]);
+  });
+});
---
+++ src/algoMonster/backTracking/phoneNumberPermutation.ts
@@ -0,0 +1,31 @@
+const KEYBOARD = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+};
+
+export function phoneNumberPermutation(digits: string): string[] {
+  function dfs(path: string[], res: string[]) {
+    if (path.length === digits.length) {
+      res.push(path.join(''));
+      return;
+    }
+
+    const nextNumber = digits.charAt(path.length);
+
+    for (const letter of KEYBOARD[nextNumber]) {
+      path.push(letter);
+      dfs(path, res);
+      path.pop();
+    }
+  }
+
+  const res = [];
+  dfs([], res);
+  return res;
+}
