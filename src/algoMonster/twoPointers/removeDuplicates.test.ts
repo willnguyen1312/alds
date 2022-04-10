a1b4dde2a8c22e446871305582d9c1711730d460
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 16:37:34 +0700

    Solve removeDuplicates 🎉

---
+++ src/algoMonster/twoPointers/removeDuplicates.test.ts
@@ -0,0 +1,7 @@
+import { removeDuplicates } from './removeDuplicates';
+
+describe('removeDuplicates function', () => {
+  it('should work', () => {
+    expect(removeDuplicates([1, 1, 2, 2, 3, 3])).toEqual([1, 2, 3]);
+  });
+});
