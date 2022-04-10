4f81c5a51597cbd0f3199484c04fc842bb782cb7
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 08:27:26 +0700

    Solve binarySearch 🎉

---
+++ src/algoMonster/binarySearch.test.ts
@@ -0,0 +1,8 @@
+import { binarySearch } from './binarySearch';
+
+describe('binarySearch function', () => {
+  it('should work', () => {
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)).toBe(4);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)).toBe(0);
+  });
+});
