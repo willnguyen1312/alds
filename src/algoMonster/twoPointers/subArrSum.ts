ad45c16f7234ff3f664b565f85a87434d44397af
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 17:46:32 +0700

    Solve subarraySumTotal 🎉

--- src/algoMonster/twoPointers/subArrSum.ts
+++ src/algoMonster/twoPointers/subArrSum.ts
@@ -11,3 +11,25 @@ export function subArrSum(arr: number[], target: number): number[] {
     prefixSums.set(curSum, i + 1);
   }
 }
+
+export function subarraySumTotal(arr: number[], target: number): number {
+  const prefixSums = new Map();
+  prefixSums.set(0, 1);
+  let curSum = 0;
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    curSum += arr[i];
+    const complement = curSum - target;
+    if (prefixSums.has(complement)) {
+      count += prefixSums.get(complement);
+    }
+
+    if (prefixSums.has(curSum)) {
+      prefixSums.set(curSum, prefixSums.get(curSum) + 1);
+    } else {
+      prefixSums.set(curSum, 1);
+    }
+  }
+  return count;
+}
