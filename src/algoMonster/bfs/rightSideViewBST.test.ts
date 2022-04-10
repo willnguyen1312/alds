a5c2c9fd19171b61fb9178167e93f605b2d14e7f
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 15:08:15 +0700

    Solve rightSideViewBST 🎉

---
+++ src/algoMonster/bfs/rightSideViewBST.test.ts
@@ -0,0 +1,8 @@
+import { rightSideViewBST, TreeNode } from './rightSideViewBST';
+
+describe('rightSideViewBST function', () => {
+  it('should work', () => {
+    const root = new TreeNode(10, new TreeNode(11), new TreeNode(12));
+    expect(rightSideViewBST(root)).toEqual([10, 12]);
+  });
+});
