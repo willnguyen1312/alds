7e8f07c721f261561c3f5c8087116778d191f998
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-10 16:48:52 +0700

    Solve twoSumSorted 🎉

---
+++ src/algoMonster/twoPointers/twoSumSorted.ts
@@ -0,0 +1,17 @@
+export function twoSumSorted(arr: number[], target: number): [number, number] {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return [-1, -1];
+}
