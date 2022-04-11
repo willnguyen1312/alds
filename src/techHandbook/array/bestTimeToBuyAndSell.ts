0d907f6528f4ee5e09f8606cc2620227eaba0826
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-11 09:37:04 +0700

    Solve maxProfit 🎉

---
+++ src/techHandbook/array/bestTimeToBuyAndSell.ts
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+function maxProfit(prices: number[]): number {
+  if (prices.length <= 1) {
+    return 0;
+  }
+
+  let left = 0;
+  let right = left + 1;
+  let result = 0;
+
+  while (right < prices.length) {
+    if (prices[left] > prices[right]) {
+      left = right;
+      right = left + 1;
+    } else {
+      result = Math.max(result, prices[right] - prices[left]);
+      right++;
+    }
+  }
+
+  return result;
+}
