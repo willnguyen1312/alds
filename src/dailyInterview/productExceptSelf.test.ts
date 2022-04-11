640be113bbb31dc9567a332aad34e8f1d6e8a382
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-11 09:56:30 +0700

    Solve productExceptSelf 🎉

--- src/dailyInterview/productExceptSelf.test.ts
+++ src/dailyInterview/productExceptSelf.test.ts
@@ -2,6 +2,6 @@ import { productExceptSelf } from './productExceptSelf';
 
 describe('productExceptSelf function', () => {
   it('should work', () => {
-    expect(productExceptSelf([1, 2, 3, 4, 5])).toEqual([120, 60, 40, 30, 24]);
+    expect(productExceptSelf([1, 2, 3, 4])).toEqual([24, 12, 8, 6]);
   });
 });
---
+++ src/techHandbook/array/productExceptSelf.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/product-of-array-except-self/
+function productExceptSelf(nums: number[]): number[] {
+  const result = [];
+  result[0] = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+
+  let right = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] = result[i] * right;
+    right *= nums[i];
+  }
+
+  return result;
+}
