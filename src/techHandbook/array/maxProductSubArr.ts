d1b939c66e2b0b978c2195fc8b8985eeb1aef2e5
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-11 10:18:45 +0700

    Solve maxProductSubArr 🎉

---
+++ src/techHandbook/array/maxProductSubArr.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-product-subarray/
+function maxProduct(nums: number[]): number {
+  let minEndingHere = nums[0];
+  let maxEndingHere = nums[0];
+  let result = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    const args = [nums[i], minEndingHere * nums[i], maxEndingHere * nums[i]];
+
+    minEndingHere = Math.min(...args);
+    maxEndingHere = Math.max(...args);
+    result = Math.max(maxEndingHere, result);
+  }
+
+  return result;
+}
