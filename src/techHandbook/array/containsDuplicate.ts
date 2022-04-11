5e8cbc2657d605912c508cb40c5258db25680c31
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-11 09:39:21 +0700

    Solve containsDuplicate 🎉

---
+++ src/techHandbook/array/containsDuplicate.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/contains-duplicate/
+function containsDuplicate(nums: number[]): boolean {
+  const set = new Set();
+
+  for (const num of nums) {
+    if (set.has(num)) {
+      return true;
+    }
+    set.add(num);
+  }
+
+  return false;
+}
