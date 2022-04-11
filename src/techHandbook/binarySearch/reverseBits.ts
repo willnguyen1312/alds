c7308ce743d9276430d73b7deb555d6af4bfc6a1
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-11 13:50:04 +0700

    Solve reverseBits 🎉

---
+++ src/techHandbook/binarySearch/reverseBits.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/reverse-bits/submissions/
+function reverseBits(n: number): number {
+  const lst = n.toString(2).split('').reverse();
+
+  let i = 0;
+  let j = 31;
+  let output = 0;
+
+  while (i < lst.length) {
+    let product = 2 ** j;
+    output += Number(lst[i]) * product;
+
+    i++;
+    j--;
+  }
+
+  return output;
+}
