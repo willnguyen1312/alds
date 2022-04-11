17920be9c17603e18e67a7d0cd26c07b6f6ff054
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-11 12:45:15 +0700

    Solve canConstruct 🎉

---
+++ src/techHandbook/hashTable/canConstruct.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/ransom-note/
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  const map = new Map<string, number>();
+
+  for (const letter of ransomNote) {
+    map.set(letter, (map.get(letter) || 0) + 1);
+  }
+
+  for (const letter of magazine) {
+    if (map.has(letter)) {
+      map.set(letter, map.get(letter) - 1);
+      if (map.get(letter) === 0) {
+        map.delete(letter);
+      }
+    }
+  }
+
+  return map.size === 0;
+}
