2ef9c7143ab051908de2ebcc3449e87a0905fc48
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-12 22:05:38 +0700

    Solve bitByBit 🎉

---
+++ src/dailyByte/bitByBit.test.ts
@@ -0,0 +1,8 @@
+import { bitByBit } from './bitByBit';
+
+describe('bitByBit function', () => {
+  it('should work', () => {
+    expect(bitByBit([4, 5, 5, 1, 3, 9])).toEqual([1, 4, 3, 5, 5, 9]);
+    expect(bitByBit([5, 2, 8])).toEqual([2, 8, 5]);
+  });
+});
---
+++ src/dailyByte/bitByBit.ts
@@ -0,0 +1,45 @@
+// Given an array of integers, nums, return an array that contains nums sorted in ascending order
+// according to their number of one bits.
+// Note: If two values have the same number of one bits, they should be sorted in ascending order.
+
+export function bitByBit(nums: number[]): number[] {
+  const numsWithBinary: { ones: number; decimal: number }[] = nums.map(
+    (num) => {
+      return {
+        decimal: num,
+        ones: countNumOfOne(num),
+      };
+    }
+  );
+
+  numsWithBinary.sort((first, second) => {
+    if (first.ones < second.ones) {
+      return -1;
+    }
+
+    if (first.ones > second.ones) {
+      return 1;
+    }
+
+    return first.decimal - second.decimal;
+  });
+
+  return numsWithBinary.map((item) => item.decimal);
+}
+
+function countNumOfOne(n: number) {
+  const result: number[] = [];
+
+  while (n > 0) {
+    result.push(n % 2);
+    n = Math.floor(n / 2);
+  }
+
+  return result.reduce((acc, curr) => {
+    if (curr === 1) {
+      acc++;
+    }
+
+    return acc;
+  }, 0);
+}
