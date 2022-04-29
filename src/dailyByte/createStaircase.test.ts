843fbe697f8037bd4312b89c36d4628409c561d1
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-29 08:45:22 +0700

    Solve createStaircase 🎉

---
+++ src/dailyByte/createStaircase.test.ts
@@ -0,0 +1,7 @@
+import { createStaircase } from './createStaircase';
+
+describe('createStaircase function', () => {
+  it('should work', () => {
+    expect(createStaircase(3)).toBe(2);
+  });
+});
---
+++ src/dailyByte/createStaircase.ts
@@ -0,0 +1,17 @@
+// Given a value, N, you are asked to form a staircase.
+// The number of elements in each row of the staircase must match the current row.
+// Return the total number of full staircase rows you can create.
+
+export function createStaircase(n: number): number {
+  let result = 0;
+  let remain = n;
+  let currentRowLength = 1;
+
+  while (currentRowLength <= remain) {
+    remain -= currentRowLength;
+    result++;
+    currentRowLength++;
+  }
+
+  return result;
+}
