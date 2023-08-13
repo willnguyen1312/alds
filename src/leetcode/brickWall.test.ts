d769cf8a198a26d76a6fed0682b6072d3f973b90
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-13 14:48:09 -0300

    Add leastBricks solution xD

--- src/leetcode/brickWall.test.ts
+++ src/leetcode/brickWall.test.ts
@@ -16,3 +16,18 @@ export function leastBricks(wall: number[][]): number {
 
   return rows - maxEdge
 }
+
+
+describe('leastBricks function', () => {
+  it('should work', () => {
+    const actual = leastBricks([
+      [1, 2, 2, 1],
+      [3, 1, 2],
+      [1, 3, 2],
+      [2, 4],
+      [3, 1, 2],
+      [1, 3, 1, 1],
+    ])
+    expect(actual).toBe(2)
+  })
+})
