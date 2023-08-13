6558a0c4b46daa65018e10213c1aa2b02e04cab6
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-13 14:39:26 -0300

    Add MyHashMap solution xD

---
+++ src/leetcode/myHashMap.test.ts
@@ -0,0 +1,29 @@
+class MyHashMap {
+    map = {}
+    put(key: number, value: number): void {
+        this.map[key] = value
+    }
+
+    get(key: number): number {
+        return this.map[key] ?? -1
+    }
+
+    remove(key: number): void {
+        delete this.map[key]
+    }
+}
+
+describe('MyHashMap', () => {
+    it('should work as expecrted', () => {
+        const hashMap = new MyHashMap()
+        hashMap.put(1, 1)
+        hashMap.put(2, 2)
+        expect(hashMap.get(1)).toEqual(1)
+        expect(hashMap.get(3)).toEqual(-1)
+        hashMap.put(2, 1)
+        expect(hashMap.get(2)).toEqual(1)
+        hashMap.remove(2)
+        expect(hashMap.get(2)).toEqual(-1)
+    })
+
+})
