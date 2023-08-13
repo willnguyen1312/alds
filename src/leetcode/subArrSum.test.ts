bb2725e8e8881507ac3173943a9df65c7a0fb582
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-13 15:45:27 -0300

    Add subArraySum function xD

---
+++ src/leetcode/subArrSum.test.ts
@@ -0,0 +1,29 @@
+function subarraySum(arr: number[], target: number): number {
+    const prefixSums = new Map()
+    prefixSums.set(0, 1)
+    let curSum = 0
+    let count = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        curSum += arr[i]
+        const complement = curSum - target
+        if (prefixSums.has(complement)) {
+            count += prefixSums.get(complement)
+        }
+
+        if (prefixSums.has(curSum)) {
+            prefixSums.set(curSum, prefixSums.get(curSum) + 1)
+        } else {
+            prefixSums.set(curSum, 1)
+        }
+    }
+
+    return count
+};
+
+describe("subarraySum", () => {
+    it("should work", () => {
+        const actual = subarraySum([1, 1, 1], 2)
+        expect(actual).toBe(2)
+    })
+})
