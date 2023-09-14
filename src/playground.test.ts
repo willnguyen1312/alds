70ebe7babaed559829a8d8c0d5a8ec50dd6506e5
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-09-14 12:32:27 -0300

    Upgrade dependencies 💞

--- src/playground.test.ts
+++ src/playground.test.ts
@@ -1,13 +1,12 @@
 describe("playground", () => {
-    it("should pass", async () => {
-        async function play() {
-            for (let index = 0; index < 3; index++) {
-                await new Promise((resolve) => setTimeout(resolve, 1000))
-                console.log(index);
-            }
-        }
+  it("should pass", async () => {
+    async function play() {
+      for (let index = 0; index < 3; index++) {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        console.log(index)
+      }
+    }
 
-
-        await play()
-    })
+    await play()
+  })
 })
