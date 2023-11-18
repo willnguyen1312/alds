89d09e29f1fcc4cf5de6a2ae7c669c770a85cd0a
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-11-18 09:37:35 -0400

    Upgrade deps 🐻‍❄️

--- src/playground.test.ts
+++ src/playground.test.ts
@@ -3,7 +3,7 @@ describe("playground", () => {
     async function play() {
       for (let index = 0; index < 3; index++) {
         await new Promise((resolve) => setTimeout(resolve, 1000))
-        console.log(index)
+        // console.log(index)
       }
     }
 
